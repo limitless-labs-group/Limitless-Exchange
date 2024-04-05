4cb59599e4af5048ee974ebe046e77506ae9f67f
Max <maxsurgai@Maxs-MacBook-Pro.local>
2024-04-05 19:42:22 +0300

    ui/ux: footer & header ui improvements

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -1,22 +1,45 @@
-import { Flex, HStack, Switch, Text } from '@chakra-ui/react'
+import { colors } from '@/styles'
+import { Flex, HStack, Text } from '@chakra-ui/react'
+import { FaCircle } from 'react-icons/fa'
 
 export const Footer = () => (
   <Flex
     w={'full'}
+    // h={'44px'}
     justifyContent={'space-between'}
     alignItems={'center'}
     flexDir={{ sm: 'column-reverse', md: 'row' }}
     gap={'10px'}
     fontSize={'sm'}
-    color={'fontLight'}
-    py={{ sm: '16px', md: '20px' }}
-    px={{ sm: '16px', md: '24px' }}
+    borderTop={`1px solid ${colors.border}`}
+    py={{ sm: 3, md: 2 }}
+    px={{ sm: 4, md: 6 }}
   >
-    <Text>© 2024 Limitless Labs. All rights reserved</Text>
-    {/* <HStack>
-      <Text>Trade</Text>
-      <Switch isDisabled />
-      <Text>Create</Text>
-    </HStack> */}
+    {/* <Text>© 2024 Limitless Labs. All rights reserved</Text> */}
+    <HStack divider={<FaCircle size={'2px'} />} gap={3} flexWrap={'wrap'} justifyContent={'center'}>
+      <Text cursor={'pointer'} _hover={{ color: 'fontLight' }}>
+        About
+      </Text>
+      <Text cursor={'pointer'} _hover={{ color: 'fontLight' }}>
+        Terms
+      </Text>
+      <Text cursor={'pointer'} _hover={{ color: 'fontLight' }}>
+        Privacy
+      </Text>
+      <Text cursor={'pointer'} _hover={{ color: 'fontLight' }}>
+        Become a creator
+      </Text>
+    </HStack>
+    <HStack spacing={4}>
+      <Text cursor={'pointer'} _hover={{ color: 'fontLight' }}>
+        Discord
+      </Text>
+      <Text cursor={'pointer'} _hover={{ color: 'fontLight' }}>
+        X
+      </Text>
+      <Text cursor={'pointer'} _hover={{ color: 'fontLight' }}>
+        Warpcast
+      </Text>
+    </HStack>
   </Flex>
 )
