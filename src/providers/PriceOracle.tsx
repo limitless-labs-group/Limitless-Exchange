7ed3b36f6032846d1544e1d919d5db32c87edd4f
valentynkvasov <valentynkvasov@gmail.com>
2024-04-11 19:28:20 +0300

    fix: replace WETH to ETH price collection

--- src/providers/PriceOracle.tsx
+++ src/providers/PriceOracle.tsx
@@ -3,67 +3,67 @@ import { useQuery } from '@tanstack/react-query'
 import axios from 'axios'
 
 /**
- * Context type providing utility functions for currency conversion between USD and WETH (Wrapped Ethereum).
+ * Context type providing utility functions for currency conversion between USD and ETH (Ethereum).
  */
 type PriceOracleContextType = {
   /**
-   * Converts a given amount in USD to its equivalent in WETH.
+   * Converts a given amount in USD to its equivalent in ETH.
    * @param usd The amount in US dollars to be converted.
    * @returns The equivalent amount in Ethereum, or undefined if conversion is not possible.
    */
-  convertUsdToWeth: (usd: number) => number | undefined
+  convertUsdToEth: (usd: number) => number | undefined
 
   /**
-   * Converts a given amount in WETH to its equivalent in USD.
-   * @param weth The amount in Wrapped Ethereum to be converted.
+   * Converts a given amount in ETH to its equivalent in USD.
+   * @param eth The amount in Ethereum to be converted.
    * @returns The equivalent amount in US dollars, or undefined if conversion is not possible.
    */
-  convertWethToUsd: (weth: number) => number | undefined
+  convertEthToUsd: (eth: number) => number | undefined
 }
 
 /**
- * Context for accessing price conversion functions between USD and WETH (Wrapped Ethereum).
+ * Context for accessing price conversion functions between USD and ETH (Ethereum).
  */
 const PriceOracleContext = createContext<PriceOracleContextType | undefined>(undefined)
 
 /**
- * Provides a React context provider for price conversions between USD and WETH.
- * This component fetches the current WETH price from an Coingecko API and provides functions to convert USD to WETH and vice versa.
+ * Provides a React context provider for price conversions between USD and ETH.
+ * This component fetches the current ETH price from an Coingecko API and provides functions to convert USD to ETH and vice versa.
  * @param children The child components to be rendered within this provider.
  */
 export const PriceOracleProvider = ({ children }: React.PropsWithChildren) => {
-  const fetchWethPrice = async () => {
+  const fetchEthPrice = async () => {
     const { data } = await axios.get(
-      'https://api.coingecko.com/api/v3/simple/price?ids=weth&vs_currencies=usd'
+      'https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd'
     )
-    return data.weth.usd as number
+    return data.ethereum.usd as number
   }
 
-  const { data: fetchedWethPrice } = useQuery({
-    queryKey: ['wethPrice'],
-    queryFn: fetchWethPrice,
+  const { data: fetchedEthPrice } = useQuery({
+    queryKey: ['ethPrice'],
+    queryFn: fetchEthPrice,
     staleTime: 1000 * 60 * 5, // Data life span 60 seconds
     refetchInterval: 1000 * 60 * 5, // Refetch data every 60 seconds
     refetchOnWindowFocus: false,
     refetchOnReconnect: false,
   })
 
-  const convertWethToUsd = (usd: number) => {
-    if (!fetchedWethPrice) {
+  const convertEthToUsd = (usd: number) => {
+    if (!fetchedEthPrice) {
       return undefined
     }
-    return usd / fetchedWethPrice
+    return usd / fetchedEthPrice
   }
 
-  const convertUsdToWeth = (weth: number) => {
-    if (!fetchedWethPrice) {
+  const convertUsdToEth = (eth: number) => {
+    if (!fetchedEthPrice) {
       return undefined
     }
-    return weth * fetchedWethPrice
+    return eth * fetchedEthPrice
   }
 
   return (
-    <PriceOracleContext.Provider value={{ convertUsdToWeth, convertWethToUsd }}>
+    <PriceOracleContext.Provider value={{ convertUsdToEth, convertEthToUsd }}>
       {children}
     </PriceOracleContext.Provider>
   )
@@ -75,9 +75,9 @@ export const PriceOracleProvider = ({ children }: React.PropsWithChildren) => {
  * @returns The PriceOracleContextType with conversion functions.
  *
  * @example
- * const {convertUsdToWeth, convertWethToUsd} = usePriceOracle();
- * convertUsdToWeth(3500) ~= 1.1
- * convertWethToUsd(1.1) ~= 3500
+ * const {convertUsdToEth, convertEthToUsd} = usePriceOracle();
+ * convertUsdToEth(3500) ~= 1.1
+ * convertEthToUsd(1.1) ~= 3500
  */
 export const usePriceOracle = (): PriceOracleContextType => {
   const context = useContext(PriceOracleContext)
