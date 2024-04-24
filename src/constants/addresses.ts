da6660e6f66ba40704cb732a6dab22a3a312e82d
Max Schnaider <100528954+maxschnaider@users.noreply.github.com>
2024-04-24 16:14:55 +0300

    fix: sell 100%; format numbers to 6 decimals; price impact fixes (#30)
    Co-authored-by: Max <maxsurgai@Maxs-MacBook-Pro.local>

--- src/constants/addresses.ts
+++ src/constants/addresses.ts
@@ -2,6 +2,6 @@ import { Address } from '@/types'
 import { base, baseSepolia } from 'viem/chains'
 
 export const conditionalTokensAddress: { [chainId: number]: Address } = {
-  [base.id]: '0x7FC645Ad1e0D69E679952C40B4D2BebcD38B7018',
+  [base.id]: '0x983d539551B219dd40A034Dd63071F1C24b96fcd',
   [baseSepolia.id]: '0xBe5201F570d405b79273DCeB90AAc7210059caE7',
 }
--- src/constants/tokens.ts
+++ src/constants/tokens.ts
@@ -4,7 +4,7 @@ import { base, baseSepolia } from 'viem/chains'
 export const usdc: Token = {
   address: {
     // [base.id]: '0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913',
-    [base.id]: '0xFd9dc7e303654d910b2B30C1CaAEfa6E4EEc71Dd', // mock
+    [base.id]: '0xFd9dc7e303654d910b2B30C1CaAEfa6E4EEc71Dd', // Mock
     [baseSepolia.id]: '0xa0DdED488E88a396096745CF4F3865210b45e217',
   },
   symbol: 'USDC',
@@ -14,7 +14,8 @@ export const usdc: Token = {
 
 export const weth: Token = {
   address: {
-    [base.id]: '0x4200000000000000000000000000000000000006',
+    // [base.id]: '0x4200000000000000000000000000000000000006',
+    [base.id]: '0x0F654BAEc6Fd510309A3A8F3461F8d73e7EeF2B3', // Mock
     [baseSepolia.id]: '0x9e1cfdAdAf5631A40d9AD3f21233a177DF05b674',
   },
   symbol: 'WETH',
