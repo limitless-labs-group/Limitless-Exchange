42d3f0cbc2b71b9b36ce55fadea49537e761f344
Carlo Miguel Dy <carlomigueldy@gmail.com>
2024-05-15 18:59:30 +0800

    fix: addresses, questionId, and conditionId

--- src/constants/markets.ts
+++ src/constants/markets.ts
@@ -1,3 +1,4 @@
+import { degen, higher, regen, weth } from '@/constants/tokens'
 import { Market } from '@/types'
 import { zeroAddress } from 'viem'
 import { base, baseSepolia } from 'viem/chains'
@@ -6,21 +7,19 @@ export const markets: Market[] = [
   {
     address: {
       [base.id]: '0x4585482A258d66b16a95734E86DCA1Ea338AC100', // prod
-      // [base.id]: zeroAddress, // dev
-      [baseSepolia.id]: '0x5856Fb2BAB01b7b8330903eCb61374A9b2fb648c',
+      [baseSepolia.id]: '0x5856Fb2BAB01b7b8330903eCb61374A9b2fb648c', //testnet
     },
     questionId: {
-      [base.id]: '0x0000000000000000000000000000000000000031373135373333323233363833',
-      [baseSepolia.id]: '0x0000000000000000000000000000000000000031373135373333323233363833',
+      [base.id]: '0x0000000000000000000000000000000000000031373134333938343634363738', // prod
+      [baseSepolia.id]: '0x0000000000000000000000000000000000000031373135373333323233363833', // testnet
     },
     conditionId: {
-      [base.id]: '0xcfe3e3db08f015e99b4ec97ad3aa01ee694937ce6af526b8c17a3c844788e311', // prod
-      // [base.id]: zeroAddress, // dev
-      [baseSepolia.id]: '0xcfe3e3db08f015e99b4ec97ad3aa01ee694937ce6af526b8c17a3c844788e311',
+      [base.id]: '0x85b3752114969525a2a84dba1c1f2534bf830e64c1293a758095126a4dd165ba', // prod
+      [baseSepolia.id]: '0xcfe3e3db08f015e99b4ec97ad3aa01ee694937ce6af526b8c17a3c844788e311', // testnet
     },
     collateralToken: {
-      [base.id]: '0x9e1cfdAdAf5631A40d9AD3f21233a177DF05b674',
-      [baseSepolia.id]: '0x9e1cfdAdAf5631A40d9AD3f21233a177DF05b674',
+      [base.id]: weth.address[base.id], // prod
+      [baseSepolia.id]: weth.address[baseSepolia.id], // testnet
     },
     tokenTicker: 'WETH',
     tokenURI: '/assets/images/tokens/weth.png',
@@ -42,14 +41,20 @@ export const markets: Market[] = [
   },
   {
     address: {
-      [base.id]: '0x0e03edc2a0ba38e803dad62b31b6e6a2f4b216cc', // prod
-      // [base.id]: '0x07148374d075792850b71e5c660fc84577a004da', // dev
-      [baseSepolia.id]: '0xca8cd401a4560a0d1b158f8292b85c78cb38ffc1',
+      [base.id]: '0x0e03eDc2A0ba38E803DaD62b31b6e6A2f4b216cc', // prod
+      [baseSepolia.id]: '0xCA8CD401A4560A0D1B158f8292b85C78cB38Ffc1', // testnet
+    },
+    questionId: {
+      [base.id]: '0x0000000000000000000000000000000000000031373133383836363736333632', // prod
+      [baseSepolia.id]: '0x0000000000000000000000000000000000000031373135323436343231383432', // testnet
     },
     conditionId: {
       [base.id]: '0x33173bef347cb768b208efd547d445aec0fa1fc16f869093a7783e13ac2c2fff', // prod
-      // [base.id]: '0x48f88962935aa6fb26a819ff22dffcda8a251f4e192d988ede363fdb8734c5f3', // dev
-      [baseSepolia.id]: '0x84d5169ac2cecd75741c02d71eb85e11c51ec6d8ed442b37e4d3210fbc03192c',
+      [baseSepolia.id]: '0x84d5169ac2cecd75741c02d71eb85e11c51ec6d8ed442b37e4d3210fbc03192c', // testnet
+    },
+    collateralToken: {
+      [base.id]: weth.address[base.id], // prod
+      [baseSepolia.id]: degen.address[baseSepolia.id], // testnet
     },
     tokenURI: '/assets/images/tokens/degen.svg',
     tokenTicker: 'DEGEN',
@@ -73,13 +78,19 @@ export const markets: Market[] = [
   {
     address: {
       [base.id]: '0x2a5254e52e338228dcd82baa4374608bb884e834', // prod
-      // [base.id]: '0x4e2fb53162472fd19a0175d55c7b11e2e81a17d7', // dev
       [baseSepolia.id]: '0x1a2ba71e69a2ad9ff40555b30586bba0efcd7616',
+    },
+    questionId: {
+      [base.id]: '0x0000000000000000000000000000000000000031373133383836353239313438', // prod
+      [baseSepolia.id]: '0x0000000000000000000000000000000000000031373135323436353432323633', // testnet
     },
     conditionId: {
       [base.id]: '0xa397e6687681345146d869a05591b002039d1ed88bfc5e871bf36d26cd89e367', // prod
-      // [base.id]: '0x42bb72b37a2d8619e1a4ea0634a9df9cbaed5f5cb849cc8122972ef7714dc6a2', // dev
-      [baseSepolia.id]: '0x0779cb01e2e832bddccb7eb9a17e0b01bb005082ac10e48843ee9fffbad59b89',
+      [baseSepolia.id]: '0x97e1b00a92cf8041deb17ccf2ea22ff61bec9b8474f77fc66cf2bec33aad4d58', // testnet
+    },
+    collateralToken: {
+      [base.id]: weth.address[base.id], // prod
+      [baseSepolia.id]: regen.address[baseSepolia.id], // testnet
     },
     outcomeTokens: ['Yes', 'No'],
     tokenTicker: 'REGEN',
@@ -102,14 +113,20 @@ export const markets: Market[] = [
   },
   {
     address: {
-      [base.id]: '0xe47907110663a3f0c06c1929ea52f50bc9821d13', // prod
-      // [base.id]: '0x883ece27b8021135f494603bd5a1477a8d144c39', // dev
-      [baseSepolia.id]: '0x729873aa483ced60acb0b7ad815f14c8a7794930',
+      [base.id]: '0xE47907110663A3F0c06c1929ea52f50bC9821d13', // prod
+      [baseSepolia.id]: '0x729873AA483CEd60ACb0B7Ad815f14C8A7794930',
+    },
+    questionId: {
+      [base.id]: '0x0000000000000000000000000000000000000031373133383836363839313539', // prod
+      [baseSepolia.id]: '0x0000000000000000000000000000000000000031373135323436373136313237', // testnet
     },
     conditionId: {
       [base.id]: '0x1f7ac97795fbb425912193fc477b009cc6d1bc7b503e9dd938b8a3281093a513', // prod
-      // [base.id]: '0xc69515f490d8a89ec3bdeef2ed4a52403f3f4521c78a6db4cde0f83447e7f4d2', // dev
-      [baseSepolia.id]: '0xf0344b254c603d3d44d38629cd6b0c4c6c3c4115411d41c54e0ce2748072685a',
+      [baseSepolia.id]: '0xf0344b254c603d3d44d38629cd6b0c4c6c3c4115411d41c54e0ce2748072685a', // testnet
+    },
+    collateralToken: {
+      [base.id]: weth.address[base.id], // prod
+      [baseSepolia.id]: higher.address[baseSepolia.id], // testnet
     },
     tokenTicker: 'HIGHER',
     tokenURI: '/assets/images/tokens/higher.svg',
