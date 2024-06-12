3e26c326e35082988602bff348395f89b139a8fc
valentynkvasov <valentynkvasov@gmail.com>
2024-06-12 18:49:49 +0100

    fix: og metadata generation

--- src/app/markets/[address]/layout.tsx
+++ src/app/markets/[address]/layout.tsx
@@ -1,9 +1,33 @@
-import { defaultChain } from '@/constants'
 import { Metadata } from 'next'
-import { useMarkets } from '@/services/MarketsService'
+import { useMarket } from '@/services/MarketsService'
+import axios from 'axios'
+import { Market } from '@/types'
 
 type Props = {
   params: { address: string }
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  let market: Market | null = null
+
+  try {
+    const response = await axios.get(
+      `${process.env.NEXT_PUBLIC_BACKEND_API_URL}/markets/${params.address}`
+    )
+    market = response.data
+  } catch (error) {
+    console.error('Failed to fetch market data:', error)
+    return {}
+  }
+
+  return {
+    title: market?.title ?? 'Limitless',
+    openGraph: {
+      title: market?.title ?? 'Limitless',
+      description: market?.description ?? 'Limitless',
+      images: [market?.ogImageURI ?? ''],
+    },
+  }
 }
 
 const Layout = ({ children }: React.PropsWithChildren) => {
