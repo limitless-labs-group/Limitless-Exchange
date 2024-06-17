99af89dd04ebb2c73cbf0879b63a3df99fb0ca38
valentynkvasov <valentynkvasov@gmail.com>
2024-06-17 17:02:41 +0100

    fix: update indexer!

--- src/constants/subgraphs.ts
+++ src/constants/subgraphs.ts
@@ -2,5 +2,5 @@ import { base, baseSepolia } from 'viem/chains'
 
 export const newSubgraphURI = {
   [base.id]: 'https://indexer.bigdevenergy.link/d881731/v1/graphql',
-  [baseSepolia.id]: 'https://indexer.bigdevenergy.link/b14f3dd/v1/graphql',
+  [baseSepolia.id]: 'https://indexer.bigdevenergy.link/db9950f/v1/graphql',
 }
