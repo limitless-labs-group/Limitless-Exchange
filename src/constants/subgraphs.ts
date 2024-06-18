d815220cafb762201efc0f4d9cfd28f2da0915db
Carlo Miguel Dy <carlomigueldy@gmail.com>
2024-06-18 17:59:47 +0800

    feat: set new subgraph urls

--- src/constants/subgraphs.ts
+++ src/constants/subgraphs.ts
@@ -1,6 +1,6 @@
 import { base, baseSepolia } from 'viem/chains'
 
 export const newSubgraphURI = {
-  [base.id]: 'https://indexer.bigdevenergy.link/d881731/v1/graphql',
-  [baseSepolia.id]: 'https://indexer.bigdevenergy.link/db9950f/v1/graphql',
+  [base.id]: 'https://indexer.bigdevenergy.link/bd7abc0/v1/graphql',
+  [baseSepolia.id]: 'https://indexer.bigdevenergy.link/24f533c/v1/graphql',
 }
