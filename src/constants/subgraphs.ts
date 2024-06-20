81ec96d19c86b943ccdd0f5a4214da4e58bff6a6
Carlo Miguel Dy <carlomigueldy@gmail.com>
2024-06-20 12:59:29 +0800

    fix: update subgraph urls

--- src/constants/subgraphs.ts
+++ src/constants/subgraphs.ts
@@ -1,6 +1,6 @@
 import { base, baseSepolia } from 'viem/chains'
 
 export const newSubgraphURI = {
-  [base.id]: 'https://indexer.bigdevenergy.link/bd7abc0/v1/graphql',
-  [baseSepolia.id]: 'https://indexer.bigdevenergy.link/24f533c/v1/graphql',
+  [base.id]: 'https://indexer.bigdevenergy.link/fba11bf/v1/graphql',
+  [baseSepolia.id]: 'https://indexer.bigdevenergy.link/21947c7/v1/graphql',
 }
