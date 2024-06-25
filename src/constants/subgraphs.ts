dba620a9238b6760c5c5f5c9f11277b95218f264
Carlo Miguel Dy <carlomigueldy@gmail.com>
2024-06-25 21:53:34 +0800

    feat: set updated testnet subgraph url

--- src/constants/subgraphs.ts
+++ src/constants/subgraphs.ts
@@ -2,5 +2,5 @@ import { base, baseSepolia } from 'viem/chains'
 
 export const newSubgraphURI = {
   [base.id]: 'https://indexer.bigdevenergy.link/173a825/v1/graphql',
-  [baseSepolia.id]: 'https://indexer.bigdevenergy.link/a699e93/v1/graphql',
+  [baseSepolia.id]: 'https://indexer.bigdevenergy.link/9a459c9/v1/graphql',
 }
