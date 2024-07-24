c107236712353a899cec6a8b73f31736384aaa97
Carlo Miguel Dy <carlomigueldy@gmail.com>
2024-07-24 16:19:10 +0800

    chore: wip debugging

--- src/app/create/components/og-image-generator.tsx
+++ src/app/create/components/og-image-generator.tsx
@@ -16,25 +16,25 @@ import { useEffect, useMemo, useRef, useState } from 'react'
 import html2canvas from 'html2canvas'
 
 interface IOgImageGeneratorOptions {
-  px: string
-  p: string
-  height: string
-  width: string
+  px: number | string
+  p: number | string
+  height: number | string
+  width: number | string
   fontSize: number
   borderRadius: string
   divider: {
     borderWidth: string
   }
   logo: {
-    width: number
-    height: number
+    width: number | string
+    height: number | string
   }
 }
 
 const [backgroundColor] = ['#0000EE']
 const exportOptions: IOgImageGeneratorOptions = {
-  px: '100px',
-  p: '10px',
+  px: 100,
+  p: 10,
   height: `1100px`,
   width: `2110px`,
   fontSize: 114,
@@ -43,14 +43,14 @@ const exportOptions: IOgImageGeneratorOptions = {
     borderWidth: '5px',
   },
   logo: {
-    width: 370,
-    height: 80,
+    width: '370px',
+    height: '80px',
   },
 }
 const previewOptions: IOgImageGeneratorOptions = {
-  px: '10px',
-  p: '5px',
-  height: '280px',
+  px: 10,
+  p: 5,
+  height: 280,
   width: 'full',
   fontSize: 32,
   borderRadius: 'md',
@@ -58,8 +58,8 @@ const previewOptions: IOgImageGeneratorOptions = {
     borderWidth: '1px',
   },
   logo: {
-    width: 185,
-    height: 40,
+    width: '185px',
+    height: '40px',
   },
 }
 
@@ -77,35 +77,35 @@ export const OgImageGenerator = ({
 }: IOgImageGenerator) => {
   const theme = useTheme()
   const canvasRef = useRef<HTMLDivElement>(null)
-  const dividerRef = useRef<HTMLHRElement>(null)
-  const imgRef = useRef<HTMLImageElement>(null)
-  const categoryTextRef = useRef<HTMLDivElement>(null)
+  // const dividerRef = useRef<HTMLHRElement>(null)
+  // const imgRef = useRef<HTMLImageElement>(null)
+  // const categoryTextRef = useRef<HTMLDivElement>(null)
 
   const [image, setImage] = useState('')
-  const [generatorOptions, setGeneratorOptions] = useState<IOgImageGeneratorOptions>(previewOptions)
+  // const [generatorOptions, setGeneratorOptions] = useState<IOgImageGeneratorOptions>(previewOptions)
 
-  const handleMofidifyStyles = (options: IOgImageGeneratorOptions) => {
-    canvasRef!.current!.style.width = options.width
-    canvasRef!.current!.style.height = options.height
-    canvasRef!.current!.style.padding = options.p
-    canvasRef!.current!.style.paddingLeft = options.px
-    canvasRef!.current!.style.paddingRight = options.px
-    canvasRef!.current!.style.borderRadius = options.borderRadius
+  // const handleMofidifyStyles = (options: IOgImageGeneratorOptions) => {
+  //   canvasRef!.current!.style.width = options.width
+  //   canvasRef!.current!.style.height = options.height
+  //   canvasRef!.current!.style.padding = options.p
+  //   canvasRef!.current!.style.paddingLeft = options.px
+  //   canvasRef!.current!.style.paddingRight = options.px
+  //   canvasRef!.current!.style.borderRadius = options.borderRadius
 
-    // Modify Divider component props
-    dividerRef!.current!.style.borderWidth = options.divider.borderWidth
+  //   // Modify Divider component props
+  //   dividerRef!.current!.style.borderWidth = options.divider.borderWidth
 
-    // Modify Img component props
-    imgRef!.current!.width = options.logo.width
-    imgRef!.current!.height = options.logo.height
+  //   // Modify Img component props
+  //   imgRef!.current!.width = options.logo.width
+  //   imgRef!.current!.height = options.logo.height
 
-    // Modify Text component props
-    categoryTextRef!.current!.style.fontSize = `${options.fontSize}px`
-  }
+  //   // Modify Text component props
+  //   categoryTextRef!.current!.style.fontSize = `${options.fontSize}px`
+  // }
 
   const handleGenerateBlob = async () => {
     try {
-      handleMofidifyStyles(exportOptions)
+      // handleMofidifyStyles(exportOptions)
 
       const componentWidth = canvasRef!.current!.offsetWidth
       const componentHeight = canvasRef!.current!.offsetHeight
@@ -116,8 +116,10 @@ export const OgImageGenerator = ({
         scale,
         width: componentWidth * scale,
         height: componentHeight * scale,
+        backgroundColor: backgroundColor,
+        allowTaint: true,
       })
-      handleMofidifyStyles(previewOptions)
+      // handleMofidifyStyles(previewOptions)
 
       const imageDataUrl = canvas.toDataURL('image/png')
       setImage(imageDataUrl)
@@ -125,12 +127,14 @@ export const OgImageGenerator = ({
       canvas.toBlob((blob) => {
         if (!blob) return
         onBlobGenerated(blob)
+        handleDownloadPreviewImage()
       })
     } catch (error) {
       console.error(error)
-    } finally {
-      handleMofidifyStyles(previewOptions)
     }
+    // finally {
+    //   handleMofidifyStyles(previewOptions)
+    // }
   }
 
   const handleDownloadPreviewImage = () => {
@@ -140,9 +144,9 @@ export const OgImageGenerator = ({
     link.click()
   }
 
-  const handleClick = () => {
-    handleGenerateBlob()
-    handleDownloadPreviewImage()
+  const handleClick = async () => {
+    await handleGenerateBlob()
+    // handleDownloadPreviewImage()
   }
 
   const { firstWord, secondWord, words } = useMemo(() => {
@@ -168,58 +172,111 @@ export const OgImageGenerator = ({
   //   }
   // }, [])
 
-  interface IOgImage {
-    options: IOgImageGeneratorOptions
-  }
-  const OgImage = ({ options }: IOgImage) => (
-    <Box
-      ref={canvasRef}
-      bg={backgroundColor}
-      width={options.width}
-      height={options.height}
-      p={options.p}
-      px={options.px}
-      borderRadius={options.borderRadius}
-    >
-      <VStack w='full' h='full' display='flex'>
-        <Divider ref={dividerRef} color='white' borderWidth={options.divider.borderWidth} />
-
-        <Text fontSize={options.fontSize} color='white' w='full'>
-          {firstWord} <span style={{ fontFamily: theme.fonts.heading }}> {secondWord} </span>
-          {words.join(' ')}
-        </Text>
-
-        <Spacer />
-
-        <HStack w='full' gap='10px'>
-          <StackItem>
-            <Img
-              ref={imgRef}
-              src='/logo.png'
-              alt='Limitless Logo'
-              width={options.logo.width}
-              height={options.logo.height}
-            />
-          </StackItem>
-          <StackItem>
-            <Box px='8px' py='1px' bg='white' borderRadius='sm'>
-              <Text ref={categoryTextRef} fontSize={options.fontSize / 1.5} color={backgroundColor}>
-                /{category}
-              </Text>
-            </Box>
-          </StackItem>
-        </HStack>
-      </VStack>
-    </Box>
-  )
-
   return (
     <>
       <Tooltip hasArrow label='Click to download OG Image for preview in full size.'>
         <VStack gap='5px' w='full' onClick={handleClick} cursor='pointer'>
-          <OgImage options={previewOptions} />
+          {/* <OgImage options={previewOptions} /> */}
+          <Box
+            bg={backgroundColor}
+            width={previewOptions.width}
+            height={previewOptions.height}
+            p={previewOptions.p}
+            px={previewOptions.px}
+            borderRadius={previewOptions.borderRadius}
+          >
+            <VStack w='full' h='full' display='flex'>
+              <Divider
+                // ref={dividerRef}
+                color='white'
+                borderWidth={previewOptions.divider.borderWidth}
+              />
+
+              <Text fontSize={previewOptions.fontSize} color='white' w='full'>
+                {firstWord} <span style={{ fontFamily: theme.fonts.heading }}> {secondWord} </span>
+                {words.join(' ')}
+              </Text>
+
+              <Spacer />
+
+              <HStack w='full' gap='10px'>
+                <StackItem>
+                  <Img
+                    // ref={imgRef}
+                    src='/logo.png'
+                    alt='Limitless Logo'
+                    width={previewOptions.logo.width}
+                    height={previewOptions.logo.height}
+                  />
+                </StackItem>
+                <StackItem>
+                  <Box px='8px' py='1px' bg='white' borderRadius='sm'>
+                    <Text
+                      // ref={categoryTextRef}
+                      fontSize={previewOptions.fontSize / 1.5}
+                      color={backgroundColor}
+                    >
+                      /{category}
+                    </Text>
+                  </Box>
+                </StackItem>
+              </HStack>
+            </VStack>
+          </Box>
         </VStack>
       </Tooltip>
+
+      <Box
+        display='block'
+        w='full'
+        // position='absolute' top='-9999px'
+      >
+        <Box
+          ref={canvasRef}
+          bg={backgroundColor}
+          width={exportOptions.width}
+          height={exportOptions.height}
+          p={exportOptions.p}
+          px={exportOptions.px}
+          borderRadius={exportOptions.borderRadius}
+        >
+          <VStack w='full' h='full' display='flex'>
+            <Divider color='white' borderWidth={exportOptions.divider.borderWidth} />
+
+            <Text fontSize={exportOptions.fontSize} color='white' w='full'>
+              {firstWord} <span style={{ fontFamily: theme.fonts.heading }}> {secondWord} </span>
+              {words.join(' ')}
+            </Text>
+
+            <Spacer />
+
+            <Box display='flex' justifyContent='start' w='full'>
+              <Img
+                src='/logo.png'
+                alt='Limitless Logo'
+                width={exportOptions.logo.width}
+                height={exportOptions.logo.height}
+              />
+
+              <Box mx={5} />
+
+              <Box
+                display='flex'
+                justifyContent='center'
+                alignItems='center'
+                borderRadius='md'
+                px={5}
+                // pb={5}
+                bg='white'
+              >
+                <Text fontSize={48} color={backgroundColor}>
+                  /{category}
+                </Text>
+              </Box>
+            </Box>
+          </VStack>
+        </Box>
+      </Box>
     </>
   )
 }
