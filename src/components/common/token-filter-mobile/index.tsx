ec16e0cb597adb549b07b50a0885286b1fff09a8
valentynkvasov <valentynkvasov@gmail.com>
2024-07-24 17:32:33 +0100

    fix: color schemas

--- src/components/common/token-filter-mobile/index.tsx
+++ src/components/common/token-filter-mobile/index.tsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react'
-import { Text, Box, Button, HStack } from '@chakra-ui/react'
+import { Text, Box, HStack } from '@chakra-ui/react'
 import { useCategories } from '@/services'
 import { useTokenFilter } from '@/contexts/TokenFilterContext'
 import '@/app/style.css'
 import Link from 'next/link'
 import { useSearchParams } from 'next/navigation'
+import { paragraphMedium } from '@/styles/fonts/fonts.styles'
 
 export default function TokenFilterMobile() {
   const [section, setSection] = useState('Topics')
@@ -17,18 +18,22 @@ export default function TokenFilterMobile() {
     (categories ?? []).map((category) => {
       const _selected = category.name === searchParams?.get('category')
       return (
-        <Button
+        <Box
           bg={_selected ? 'grey.800' : 'grey.300'}
-          variant='grey'
-          key={category.id}
           onClick={() => handleCategory(category)}
+          padding='4px 8px'
+          key={category.id}
+          borderRadius='2px'
+          w='fit-content'
+          marginBottom='4px'
+          cursor='pointer'
         >
           <Link href={{ pathname: '/', query: { category: category.name } }}>
-            <Text color={_selected ? 'white' : 'black'} fontWeight={500}>
+            <Text {...paragraphMedium} color={_selected ? 'white' : 'black'} fontWeight={500}>
               /{category.name}
             </Text>
           </Link>
-        </Button>
+        </Box>
       )
     })
 
