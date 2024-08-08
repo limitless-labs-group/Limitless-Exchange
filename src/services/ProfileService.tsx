e184112d38c26260a1eed5318ae59844e5430f08
Carlo Miguel Dy <carlomigueldy@gmail.com>
2024-08-08 15:36:20 +0800

    feat: wrap up profile service, reusable on both desktop and mobile components

---
+++ src/services/ProfileService.tsx
@@ -0,0 +1,88 @@
+import { useCreateProfile, useProfile, useUpdatePfp, useUpdateProfile } from '@/hooks/profiles'
+import { useAccount } from '@/services'
+import { useCallback, useEffect, useRef, useState } from 'react'
+
+export const useProfileService = () => {
+  const { farcasterInfo, userInfo } = useAccount()
+
+  const pfpFileRef = useRef<any>()
+  const [pfpFile, setPfpFile] = useState<File | undefined>(undefined)
+  const [pfpPreview, setPfpPreview] = useState<string | undefined>(undefined)
+  const [pfpUrl, setPfpUrl] = useState<string | undefined>(undefined)
+  const [displayName, setDisplayName] = useState(userInfo?.name ?? '')
+  const [username, setUsername] = useState(farcasterInfo?.username ?? '')
+  const [bio, setBio] = useState('')
+  const [profileUpdated, setProfileUpdated] = useState<boolean>(false)
+  const [disableUpdateButton, setDisableUpdateButton] = useState<boolean>(false)
+
+  const { mutateAsync: createProfileAsync, isPending: createProfileLoading } = useCreateProfile()
+  const { mutateAsync: updateProfileAsync, isPending: updateProfileLoading } = useUpdateProfile()
+  const { mutateAsync: updatePfpAsync, isPending: updatePfpLoading } = useUpdatePfp()
+  const { data: profileData, isPending: getProfileDataLoading } = useProfile()
+
+  const profileRegistered = !!profileData
+  const updateButtonDisabled =
+    getProfileDataLoading ||
+    createProfileLoading ||
+    updateProfileLoading ||
+    profileUpdated ||
+    disableUpdateButton
+  const updateButtonLoading = createProfileLoading || updateProfileLoading
+
+  useEffect(() => {
+    if (!profileData) return
+    setPfpUrl(profileData.pfpUrl)
+    setDisplayName(profileData.displayName)
+    setUsername(profileData.username)
+    setBio(profileData.bio ?? '')
+  }, [profileData])
+
+  useEffect(() => {
+    if (pfpFile) {
+      const previewUrl = URL.createObjectURL(pfpFile)
+      setPfpPreview(previewUrl)
+
+      if (profileData) {
+        updatePfpAsync(pfpFile)
+      }
+
+      // Clean up the preview URL when the component unmounts or the file changes
+      return () => URL.revokeObjectURL(previewUrl)
+    }
+  }, [pfpFile, profileData])
+
+  const handleUpdateProfile = useCallback(async () => {
+    try {
+      profileRegistered
+        ? await updateProfileAsync({ displayName, username, bio })
+        : await createProfileAsync({ displayName, username, bio })
+      setProfileUpdated(true)
+      setTimeout(() => {
+        setDisableUpdateButton(true)
+      }, 3_000)
+    } catch (error) {
+      console.error(error)
+      setProfileUpdated(false)
+      setDisableUpdateButton(false)
+    }
+  }, [profileRegistered, displayName, username, bio])
+
+  return {
+    handleUpdateProfile,
+    updateButtonLoading,
+    updateButtonDisabled,
+    disableUpdateButton,
+    pfpPreview,
+    profileUpdated,
+    pfpUrl,
+    pfpFileRef,
+    updatePfpLoading,
+    setPfpFile,
+    displayName,
+    setDisplayName,
+    username,
+    setUsername,
+    bio,
+    setBio,
+  }
+}
