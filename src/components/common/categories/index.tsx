3cfd15b3f5ccaa5a2bd96bf85b1f621f70d08548
valentynkvasov <valentynkvasov@gmail.com>
2024-08-20 11:45:19 +0100

    add: temporal stub to hide /Other markets for desktop

--- src/components/common/categories/index.tsx
+++ src/components/common/categories/index.tsx
@@ -26,37 +26,39 @@ export default function CategoryFilter() {
       >
         Topics
       </Text>
-      {categories?.map((category) => (
-        <Box
-          bg={category.name === searchParams?.get('category') ? 'grey.800' : 'grey.300'}
-          padding='2px 4px'
-          key={category.id}
-          borderRadius='2px'
-          w='fit-content'
-          marginBottom='4px'
-          cursor='pointer'
-          _hover={{
-            bg: category.name === searchParams?.get('category') ? 'grey.800' : 'grey.400',
-          }}
-        >
-          <Link
-            href={{ pathname: '/', query: { category: category.name } }}
-            onClick={() => {
-              trackClicked(ClickEvent.CategoryClicked, {
-                name: category.name,
-                page: pageName,
-              })
+      {categories
+        ?.filter((category) => category.name !== 'Other')
+        ?.map((category) => (
+          <Box
+            bg={category.name === searchParams?.get('category') ? 'grey.800' : 'grey.300'}
+            padding='2px 4px'
+            key={category.id}
+            borderRadius='2px'
+            w='fit-content'
+            marginBottom='4px'
+            cursor='pointer'
+            _hover={{
+              bg: category.name === searchParams?.get('category') ? 'grey.800' : 'grey.400',
             }}
           >
-            <Text
-              color={category.name === searchParams?.get('category') ? 'grey.50' : 'grey.800'}
-              fontWeight={500}
+            <Link
+              href={{ pathname: '/', query: { category: category.name } }}
+              onClick={() => {
+                trackClicked(ClickEvent.CategoryClicked, {
+                  name: category.name,
+                  page: pageName,
+                })
+              }}
             >
-              /{category.name}
-            </Text>
-          </Link>
-        </Box>
-      ))}
+              <Text
+                color={category.name === searchParams?.get('category') ? 'grey.50' : 'grey.800'}
+                fontWeight={500}
+              >
+                /{category.name}
+              </Text>
+            </Link>
+          </Box>
+        ))}
     </Box>
   )
 }
