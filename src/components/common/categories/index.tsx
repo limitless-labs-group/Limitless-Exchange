afe9d25a8d01f2fb9585be572d371aa6ce7debe0
valentynkvasov <valentynkvasov@gmail.com>
2024-08-20 11:57:43 +0100

    add: get back Other filter

--- src/components/common/categories/index.tsx
+++ src/components/common/categories/index.tsx
@@ -26,39 +26,37 @@ export default function CategoryFilter() {
       >
         Topics
       </Text>
-      {categories
-        ?.filter((category) => category.name !== 'Other')
-        ?.map((category) => (
-          <Box
-            bg={category.name === searchParams?.get('category') ? 'grey.800' : 'grey.300'}
-            padding='2px 4px'
-            key={category.id}
-            borderRadius='2px'
-            w='fit-content'
-            marginBottom='4px'
-            cursor='pointer'
-            _hover={{
-              bg: category.name === searchParams?.get('category') ? 'grey.800' : 'grey.400',
+      {categories?.map((category) => (
+        <Box
+          bg={category.name === searchParams?.get('category') ? 'grey.800' : 'grey.300'}
+          padding='2px 4px'
+          key={category.id}
+          borderRadius='2px'
+          w='fit-content'
+          marginBottom='4px'
+          cursor='pointer'
+          _hover={{
+            bg: category.name === searchParams?.get('category') ? 'grey.800' : 'grey.400',
+          }}
+        >
+          <Link
+            href={{ pathname: '/', query: { category: category.name } }}
+            onClick={() => {
+              trackClicked(ClickEvent.CategoryClicked, {
+                name: category.name,
+                page: pageName,
+              })
             }}
           >
-            <Link
-              href={{ pathname: '/', query: { category: category.name } }}
-              onClick={() => {
-                trackClicked(ClickEvent.CategoryClicked, {
-                  name: category.name,
-                  page: pageName,
-                })
-              }}
+            <Text
+              color={category.name === searchParams?.get('category') ? 'grey.50' : 'grey.800'}
+              fontWeight={500}
             >
-              <Text
-                color={category.name === searchParams?.get('category') ? 'grey.50' : 'grey.800'}
-                fontWeight={500}
-              >
-                /{category.name}
-              </Text>
-            </Link>
-          </Box>
-        ))}
+              /{category.name}
+            </Text>
+          </Link>
+        </Box>
+      ))}
     </Box>
   )
 }
