b32caa0d8f982f1df46504e7ade6f33aa60f632e
valentynkvasov <valentynkvasov@gmail.com>
2024-10-09 19:20:31 +0100

    remove: link from draft market

--- src/app/draft/queue/components/draft-card.tsx
+++ src/app/draft/queue/components/draft-card.tsx
@@ -56,101 +56,99 @@ export const DraftMarketCard = ({ market, isChecked, onToggle }: DraftMarketSing
           marginTop='4px'
         />
 
-        <NextLink href={`/markets/${market.id}`} passHref>
-          <Box as='a' width='100%'>
-            <Stack gap='5px' width='100%'>
-              <HStack justifyContent='space-between' mb='5px' alignItems='flex-start'>
+        <Box as='a' width='100%'>
+          <Stack gap='5px' width='100%'>
+            <HStack justifyContent='space-between' mb='5px' alignItems='flex-start'>
+              <Text {...paragraphMedium} color={colors.main}>
+                {market.title}
+              </Text>
+              <HStack gap={1} color={colors.main}>
                 <Text {...paragraphMedium} color={colors.main}>
-                  {market.title}
+                  {market.draftMetadata.initialProbability * 100}%
                 </Text>
-                <HStack gap={1} color={colors.main}>
-                  <Text {...paragraphMedium} color={colors.main}>
-                    {market.draftMetadata.initialProbability * 100}%
+                <Box w='16px' h='16px' display='flex' alignItems='center' justifyContent='center'>
+                  <Box
+                    h='100%'
+                    w='100%'
+                    borderRadius='100%'
+                    bg={`conic-gradient(${colors.main} ${
+                      market.draftMetadata.initialProbability * 100
+                    }% 10%, ${colors.chartBg} ${
+                      market.draftMetadata.initialProbability * 100
+                    }% 100%)`}
+                  />
+                </Box>
+              </HStack>
+            </HStack>
+
+            <HStack alignItems='flex-start'>
+              <Text {...paragraphMedium} color={colors.main}>
+                {market.description}
+              </Text>
+            </HStack>
+
+            <HStack gap='8px' flexWrap='wrap'>
+              <ChakraImage
+                width={6}
+                height={6}
+                src={market?.creator.imageURI ?? '/assets/images/logo.svg'}
+                alt='creator'
+                borderRadius={'2px'}
+              />
+              <Link href={market?.creator.link} isExternal>
+                <Text color='grey.500'>{market?.creator.name}</Text>
+              </Link>
+              {market?.tags?.map((tag: any) => (
+                <Text color='grey.500' key={tag.id}>
+                  #{tag.name}
+                </Text>
+              ))}
+            </HStack>
+
+            <HStack justifyContent='space-between' alignItems='flex-end' flexDirection={'row'}>
+              <HStack gap={'16px'} flexDirection={'row'} w='full'>
+                {/* Liquidity */}
+                <HStack w={'unset'} justifyContent={'unset'}>
+                  <HStack color={colors.secondary} gap='4px'>
+                    <LiquidityIcon width={16} height={16} />
+                    <Text {...paragraphMedium} color={colors.secondary}>
+                      Liquidity
+                    </Text>
+                  </HStack>
+                  <Text {...paragraphRegular} color={colors.main}>
+                    {NumberUtil.formatThousands(market.draftMetadata.liquidity, 6) +
+                      ' ' +
+                      market.collateralToken.symbol}
                   </Text>
-                  <Box w='16px' h='16px' display='flex' alignItems='center' justifyContent='center'>
-                    <Box
-                      h='100%'
-                      w='100%'
-                      borderRadius='100%'
-                      bg={`conic-gradient(${colors.main} ${
-                        market.draftMetadata.initialProbability * 100
-                      }% 10%, ${colors.chartBg} ${
-                        market.draftMetadata.initialProbability * 100
-                      }% 100%)`}
-                    />
-                  </Box>
+                </HStack>
+
+                <HStack w={'unset'} justifyContent={'unset'}>
+                  <HStack color={colors.secondary} gap='4px'>
+                    <LiquidityIcon width={16} height={16} />
+                    <Text {...paragraphMedium} color={colors.secondary}>
+                      Deadline
+                    </Text>
+                  </HStack>
+                  <Text {...paragraphRegular} color={colors.main}>
+                    {market.deadline}
+                  </Text>
+                </HStack>
+
+                <HStack w={'unset'} justifyContent={'unset'}>
+                  <HStack color={colors.secondary} gap='4px'>
+                    <LiquidityIcon width={16} height={16} />
+                    <Text {...paragraphMedium} color={colors.secondary}>
+                      Market Fee
+                    </Text>
+                  </HStack>
+                  <Text {...paragraphRegular} color={colors.main}>
+                    {market.draftMetadata.fee}%
+                  </Text>
                 </HStack>
               </HStack>
-
-              <HStack alignItems='flex-start'>
-                <Text {...paragraphMedium} color={colors.main}>
-                  {market.description}
-                </Text>
-              </HStack>
-
-              <HStack gap='8px' flexWrap='wrap'>
-                <ChakraImage
-                  width={6}
-                  height={6}
-                  src={market?.creator.imageURI ?? '/assets/images/logo.svg'}
-                  alt='creator'
-                  borderRadius={'2px'}
-                />
-                <Link href={market?.creator.link} isExternal>
-                  <Text color='grey.500'>{market?.creator.name}</Text>
-                </Link>
-                {market?.tags?.map((tag: any) => (
-                  <Text color='grey.500' key={tag.id}>
-                    #{tag.name}
-                  </Text>
-                ))}
-              </HStack>
-
-              <HStack justifyContent='space-between' alignItems='flex-end' flexDirection={'row'}>
-                <HStack gap={'16px'} flexDirection={'row'} w='full'>
-                  {/* Liquidity */}
-                  <HStack w={'unset'} justifyContent={'unset'}>
-                    <HStack color={colors.secondary} gap='4px'>
-                      <LiquidityIcon width={16} height={16} />
-                      <Text {...paragraphMedium} color={colors.secondary}>
-                        Liquidity
-                      </Text>
-                    </HStack>
-                    <Text {...paragraphRegular} color={colors.main}>
-                      {NumberUtil.formatThousands(market.draftMetadata.liquidity, 6) +
-                        ' ' +
-                        market.collateralToken.symbol}
-                    </Text>
-                  </HStack>
-
-                  <HStack w={'unset'} justifyContent={'unset'}>
-                    <HStack color={colors.secondary} gap='4px'>
-                      <LiquidityIcon width={16} height={16} />
-                      <Text {...paragraphMedium} color={colors.secondary}>
-                        Deadline
-                      </Text>
-                    </HStack>
-                    <Text {...paragraphRegular} color={colors.main}>
-                      {market.deadline}
-                    </Text>
-                  </HStack>
-
-                  <HStack w={'unset'} justifyContent={'unset'}>
-                    <HStack color={colors.secondary} gap='4px'>
-                      <LiquidityIcon width={16} height={16} />
-                      <Text {...paragraphMedium} color={colors.secondary}>
-                        Market Fee
-                      </Text>
-                    </HStack>
-                    <Text {...paragraphRegular} color={colors.main}>
-                      {market.draftMetadata.fee}%
-                    </Text>
-                  </HStack>
-                </HStack>
-              </HStack>
-            </Stack>
-          </Box>
-        </NextLink>
+            </HStack>
+          </Stack>
+        </Box>
       </HStack>
     </Paper>
   )
