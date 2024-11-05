7c50184111e71d830f96a36484db1deceeba2546
evaries <nadtochii.e@gmail.com>
2024-11-05 15:14:31 +0800

    added y axis to chart

--- src/app/(markets)/markets/[address]/components/market-price-chart.tsx
+++ src/app/(markets)/markets/[address]/components/market-price-chart.tsx
@@ -71,7 +71,7 @@ export const MarketPriceChart = ({ marketGroup, market }: IMarketPriceChart) => 
       },
       height: 230,
       backgroundColor: colors.grey['100'],
-      marginLeft: 0,
+      marginLeft: 50,
       marginRight: 0,
     },
     title: {
@@ -99,7 +99,29 @@ export const MarketPriceChart = ({ marketGroup, market }: IMarketPriceChart) => 
       },
     },
     yAxis: {
-      visible: false,
+      visible: true,
+      min: 0,
+      max: 100,
+      tickInterval: 25,
+      title: {
+        text: 'Percentage (%)',
+        style: {
+          color: colors.grey['600'],
+        },
+      },
+      labels: {
+        format: '{value}%',
+        style: {
+          fontFamily: 'Helvetica Neue',
+          fontSize: isMobile ? '14px' : '12px',
+          color: colors.grey['400'],
+        },
+      },
+      gridLineColor: colors.grey['200'],
+      lineWidth: 1,
+      lineColor: colors.grey['200'],
+      tickWidth: 1,
+      tickColor: colors.grey['200'],
     },
     legend: {
       enabled: false,
@@ -171,15 +193,115 @@ export const MarketPriceChart = ({ marketGroup, market }: IMarketPriceChart) => 
     ],
   })
 
+  // const getChartOptions = (data: number[][] | undefined): Highcharts.Options => ({
+  //   chart: {
+  //     zooming: {
+  //       type: 'x',
+  //     },
+  //     height: 230,
+  //     backgroundColor: colors.grey['100'],
+  //     marginLeft: 0,
+  //     marginRight: 0,
+  //   },
+  //   title: {
+  //     text: undefined,
+  //   },
+  //   xAxis: {
+  //     type: 'datetime',
+  //     ordinal: false,
+  //     tickPosition: 'outside',
+  //     lineColor: colors.grey['200'],
+  //     tickColor: colors.grey['200'],
+  //     tickLength: 0,
+  //     labels: {
+  //       step: 0,
+  //       rotation: 0,
+  //       align: 'center',
+  //       style: {
+  //         fontFamily: 'Helvetica Neue',
+  //         fontSize: isMobile ? '14px' : '12px',
+  //         color: colors.grey['400'],
+  //       },
+  //       formatter: function () {
+  //         return Highcharts.dateFormat('%b %e', Number(this.value))
+  //       },
+  //     },
+  //   },
+  //   yAxis: {
+  //     visible: false,
+  //   },
+  //   legend: {
+  //     enabled: false,
+  //   },
+  //   credits: {
+  //     enabled: false,
+  //   },
+  //   tooltip: {
+  //     shared: true,
+  //     formatter: function () {
+  //       return `YES: <b>${Number(this.y).toFixed(2)}%</b>`
+  //     },
+  //   },
+  //   plotOptions: {
+  //     series: {
+  //       lineWidth: 4,
+  //       marker: {
+  //         enabled: false,
+  //       },
+  //       point: {
+  //         events: {
+  //           mouseOver: function () {
+  //             //@ts-ignore
+  //             setYesDate(Highcharts.dateFormat('%B %e, %Y %I:%M %p', Number(this.x)))
+  //             //@ts-ignore
+  //             setYesChance(this.y.toFixed(2))
+  //           },
+  //         },
+  //       },
+  //     },
+  //     area: {
+  //       fillColor: {
+  //         linearGradient: {
+  //           x1: 0,
+  //           y1: 0,
+  //           x2: 0,
+  //           y2: 1,
+  //         },
+  //         stops: [
+  //           //@ts-ignore
+  //           [0, Highcharts.color('#198020').setOpacity(0.3).get('rgba')],
+  //           //@ts-ignore
+  //           [1, Highcharts.color('#198020').setOpacity(0).get('rgba')],
+  //         ],
+  //         brighten: 0.2,
+  //       },
+  //       marker: {
+  //         radius: 2,
+  //       },
+  //       lineWidth: 1,
+  //       states: {
+  //         hover: {
+  //           lineWidth: 1,
+  //         },
+  //       },
+  //       threshold: null,
+  //     },
+  //   },
+  //   series: [
+  //     {
+  //       type: 'area',
+  //       name: 'Price',
+  //       data: data,
+  //       turboThreshold: 2000,
+  //       boostThreshold: 2000,
+  //       color: '#238020',
+  //       lineWidth: 2,
+  //     },
+  //   ],
+  // })
+
   // React Query to fetch the price data
   const { data: prices, refetch: refetchPrices } = useMarketPriceHistory(market?.address)
-
-  // const initialYesChance = useMemo(() => {
-  //   if (market?.prices) {
-  //     return market.prices[0].toFixed(2)
-  //   }
-  //   return '50.00'
-  // }, [market?.prices])
 
   const chartData = useMemo(() => {
     const _prices: number[][] = prices ?? []
@@ -195,87 +317,11 @@ export const MarketPriceChart = ({ marketGroup, market }: IMarketPriceChart) => 
         })
       : _prices
 
-    // special case hotfix
-    // const special = {
-    //   [getAddress('0xD0BC7FCea7500d485329e0aaE36e0512815684BF')]: {
-    //     index: 0,
-    //     timestamp: 1722745928000, // aug 4 2024
-    //     exists: true,
-    //   },
-    // }
-    // if (special[getAddress(marketAddr)]?.exists) {
-    //   const _index = special[getAddress(marketAddr)].index
-    //
-    //   if (data[_index]) {
-    //     data[_index][0] = special[getAddress(marketAddr)].timestamp
-    //
-    //     for (let index = 0; index < Array.from({ length: 10 }).length; index++) {
-    //       data.splice(index + 1, 0, [data[index][0] + ONE_HOUR, data[index][1]])
-    //     }
-    //   }
-    // }
-
     return data
   }, [prices, winningIndex, resolved])
 
   return (
     <Paper my='20px' py='8px' px={0} bg='grey.100' borderRadius='8px'>
-      {/*{marketGroup ? (*/}
-      {/*  <Menu isOpen={isMarketListOpen} onClose={onCloseMarketList} variant='transparent'>*/}
-      {/*    <MenuButton*/}
-      {/*      as={Button}*/}
-      {/*      onClick={() => {*/}
-      {/*        trackClicked(ClickEvent.ChangeMarketInGroupClicked, {*/}
-      {/*          marketGroup,*/}
-      {/*        })*/}
-      {/*        onOpenMarketList()*/}
-      {/*      }}*/}
-      {/*      p={0}*/}
-      {/*      h='unset'*/}
-      {/*    >*/}
-      {/*      <HStack gap={isMobile ? '16px' : '8px'} color='green.500'>*/}
-      {/*        <Text {...paragraphMedium} color='green.500'>*/}
-      {/*          {market?.title}*/}
-      {/*        </Text>*/}
-      {/*        <HStack gap={isMobile ? '8px' : '4px'}>*/}
-      {/*          <ThumbsUpIcon width={16} height={16} />*/}
-      {/*          <Text {...paragraphMedium} color='green.500'>*/}
-      {/*            {!resolved ? outcomeTokensPercent?.[0] : winningIndex === 0 ? 100 : 0}% YES*/}
-      {/*          </Text>*/}
-      {/*          <Box*/}
-      {/*            transform={`rotate(${isMarketListOpen ? '180deg' : 0})`}*/}
-      {/*            transition='0.5s'*/}
-      {/*            color='green.500'*/}
-      {/*          >*/}
-      {/*            <ChevronDownIcon width='16px' height='16px' />*/}
-      {/*          </Box>*/}
-      {/*        </HStack>*/}
-      {/*      </HStack>*/}
-      {/*    </MenuButton>*/}
-      {/*    <MenuList borderRadius='2px' zIndex={2} marginTop='-8px'>*/}
-      {/*      {marketGroup.markets.map((market) => (*/}
-      {/*        <MenuItem*/}
-      {/*          onClick={() => {*/}
-      {/*            setSelectedMarket && setSelectedMarket(market)*/}
-      {/*          }}*/}
-      {/*          key={market.address}*/}
-      {/*        >*/}
-      {/*          {market.title}*/}
-      {/*        </MenuItem>*/}
-      {/*      ))}*/}
-      {/*    </MenuList>*/}
-      {/*  </Menu>*/}
-      {/*) : (*/}
-      {/*  <HStack gap={'4px'}>*/}
-      {/*    <Text {...headline} color='grey.800'>*/}
-      {/*      {!resolved ? outcomeTokensPercent?.[0] : winningIndex === 0 ? 100 : 0}%*/}
-      {/*    </Text>*/}
-      {/*    <Text {...headline} color='grey.800'>*/}
-      {/*      Yes*/}
-      {/*    </Text>*/}
-      {/*    /!*<ChevronDownIcon width={16} height={16} />*!/*/}
-      {/*  </HStack>*/}
-      {/*)}*/}
       <Box px='8px'>
         <HStack>
           <VStack gap={-1} alignItems={'flex-start'}>
