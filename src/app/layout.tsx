70b0f0692c587ada514941d1963fe0009dc294a7
ynadtochii <nadtochii.e@gmail.com>
2024-11-05 21:29:26 +0800

    added apple touch icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,10 @@ const RootLayout = ({ children }: PropsWithChildren) => {
   return (
     <html lang='en'>
       <head>
+        <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
+        <meta name='apple-mobile-web-app-title' content='Limitless' />
+        <meta name='apple-mobile-web-app-capable' content='yes' />
+        <meta name='apple-mobile-web-app-status-bar-style' content='default' />
         <meta name='google' content='notranslate' />
         <meta name='description' content='Daily prediction markets on Base' />
       </head>
