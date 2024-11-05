f720437ac56ea47e90778d8d0f3008a7fc569a4f
ynadtochii <nadtochii.e@gmail.com>
2024-11-05 22:05:25 +0800

    added manifest

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,7 @@ const RootLayout = ({ children }: PropsWithChildren) => {
     <html lang='en'>
       <head>
         <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
+        <link rel='manifest' href='/manifest.json' />
         <meta name='apple-mobile-web-app-title' content='Limitless' />
         <meta name='apple-mobile-web-app-capable' content='yes' />
         <meta name='apple-mobile-web-app-status-bar-style' content='default' />
