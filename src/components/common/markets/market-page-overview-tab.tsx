7971f94428500f2383ce925a8c575aac31034e81
valentynkvasov <valentynkvasov@gmail.com>
2024-11-08 16:42:33 +0000

    add: "resolution is decentralised" label

--- src/components/common/markets/market-page-overview-tab.tsx
+++ src/components/common/markets/market-page-overview-tab.tsx
@@ -6,6 +6,7 @@ import MarketGroupPositions from '@/app/(markets)/market-group/[slug]/components
 import { MarketPositions } from '@/app/(markets)/markets/[address]/components'
 import { useTradingService } from '@/services'
 import { paragraphRegular } from '@/styles/fonts/fonts.styles'
+import { Category } from '@/types'
 import { parseTextWithLinks } from '@/utils/string'
 
 export default function MarketPageOverviewTab() {
@@ -20,16 +21,22 @@ export default function MarketPageOverviewTab() {
       )}
       <MarketGroupPredictions />
       <Box w={isMobile ? 'full' : 'fit-content'} mt='16px' pb={isMobile ? '64px' : 0}>
-        <NextLink
-          href='https://www.notion.so/limitlesslabs/Limitless-Docs-0e59399dd44b492f8d494050969a1567?pvs=4#5dd6f962c66044eaa00e28d2c61b92bb'
-          target='_blank'
-          rel='noopener'
-          passHref
-        >
+        {market?.category !== 'Lumy' ? (
+          <NextLink
+            href='https://www.notion.so/limitlesslabs/Limitless-Docs-0e59399dd44b492f8d494050969a1567?pvs=4#5dd6f962c66044eaa00e28d2c61b92bb'
+            target='_blank'
+            rel='noopener'
+            passHref
+          >
+            <Link variant='textLinkSecondary' {...paragraphRegular} isExternal color='grey.500'>
+              Resolution is centralised
+            </Link>
+          </NextLink>
+        ) : (
           <Link variant='textLinkSecondary' {...paragraphRegular} isExternal color='grey.500'>
-            Resolution is centralised
+            Resolution is decentralised
           </Link>
-        </NextLink>
+        )}
         <Text {...paragraphRegular} color='grey.500' as='span'>
           {' '}
           and made by the Limitless team
