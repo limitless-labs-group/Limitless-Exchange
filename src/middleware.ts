912bb707067205a5a95f466043f3dc9def34b803
ynadtochii <nadtochii.e@gmail.com>
2024-11-20 23:30:52 +0800

    revert changes for redirect

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,12 +5,8 @@ export const config = {
   matcher: '/:path*', // This matches all routes
 }
 
-const baseUrl = process.env.NEXT_PUBLIC_BACKEND_API_URL
-const home = process.env.NEXT_PUBLIC_FRAME_URL
-
 export async function middleware(req: NextRequest) {
   const { country } = geolocation(req)
-  const { pathname } = req.nextUrl
 
   const response = NextResponse.next()
 
@@ -25,23 +21,5 @@ export async function middleware(req: NextRequest) {
     response.cookies.delete('limitless_geo')
   }
 
-  if (!baseUrl || !home) {
-    console.error('Missing required environment variables.')
-    return response
-  }
-
-  if (pathname.startsWith('/draft')) {
-    try {
-      const authResponse = await fetch(`${baseUrl}/auth/verify-auth`, {
-        method: 'GET',
-      })
-
-      if (!authResponse.ok && authResponse.status === 401) {
-        return NextResponse.redirect(home)
-      }
-    } catch (error) {
-      console.error('Error verifying auth:', error)
-    }
-  }
   return response
 }
