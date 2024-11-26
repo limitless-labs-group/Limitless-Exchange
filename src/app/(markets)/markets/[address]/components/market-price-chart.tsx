c786360eb986b51ae94e61a1f954cbec6f55d06c
oleksandrtarapata <oleksandr@limitless.network>
2024-11-26 15:10:28 +0000

    fixed graph display

--- src/app/(markets)/markets/[address]/components/market-price-chart.tsx
+++ src/app/(markets)/markets/[address]/components/market-price-chart.tsx
@@ -51,7 +51,7 @@ export const MarketPriceChart = () => {
       lineColor: colors.grey['200'],
       tickColor: colors.grey['200'],
       tickLength: 0,
-      max: data ? data[data.length - 1][0] : new Date().getTime(),
+      max: market?.expirationTimestamp,
       labels: {
         step: 0,
         rotation: 0,
