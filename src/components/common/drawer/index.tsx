f80ce09677df5caf7b5f10b4e0e89e9a72c299c9
ynadtochii <nadtochii.e@gmail.com>
2024-11-29 16:39:22 +0800

    temp fix height

--- src/components/common/drawer/index.tsx
+++ src/components/common/drawer/index.tsx
@@ -132,7 +132,7 @@ export default function MobileDrawer({
       background: bgColor,
       display: 'flex',
       flexDirection: 'column',
-      height: 'calc(100dvh - 36px)',
+      // height: 'calc(100dvh - 36px)',
       position: 'fixed',
       bottom: 0,
       left: 0,
