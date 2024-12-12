da7a251b2647a4e73484d1da286c33ce5900aa65
oleksandrtarapata <oleksandr@limitless.network>
2024-12-12 18:48:30 +0000

    feedback updates

--- src/constants/application.ts
+++ src/constants/application.ts
@@ -4,6 +4,6 @@ export const LOCAL_STORAGE_VERSION_NAME = 'limitless_exchange_build_version'
 export const POLLING_INTERVAL = 5000 //in ms
 
 export const draftMarketAddress = {
-  [base.id]: '0x4c3C0583dEb9E081b2481FB26E8e5aD914Dcee23',
-  [baseSepolia.id]: '0x32e52896663De88a65c2D94917b006404415A89f',
+  [base.id]: '0x32e52896663De88a65c2D94917b006404415A89f',
+  [baseSepolia.id]: '0x4c3C0583dEb9E081b2481FB26E8e5aD914Dcee23',
 }
