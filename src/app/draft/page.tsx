9c9ebf4eab6165c13e90aa70342da12e86847950
ynadtochii <nadtochii.e@gmail.com>
2024-12-12 22:47:49 +0800

    updates

--- src/app/draft/page.tsx
+++ src/app/draft/page.tsx
@@ -14,50 +14,55 @@ import CopyIcon from '@/resources/icons/copy-icon.svg'
 import LoadingIcon from '@/resources/icons/loader-icon.svg'
 import PlusIcon from '@/resources/icons/plus-square-icon.svg'
 
-const CreateOwnMarketPage = () => {
+type Tab = {
+  title: string
+  icon: React.JSX.Element
+  component: React.FC
+  param: string
+}
+
+const useTabLogic = (tabs: Tab[]) => {
   const router = useRouter()
   const searchParams = useSearchParams()
   const [activeIndex, setActiveIndex] = useState(0)
 
   useEffect(() => {
     const tab = searchParams.get('tab')
-    switch (tab) {
-      case 'queue':
-        setActiveIndex(1)
-        break
-      case 'recent':
-        setActiveIndex(2)
-        break
-      default:
-        setActiveIndex(0)
-    }
+    const index = tabs.findIndex((t) => t.param === tab)
+    setActiveIndex(index !== -1 ? index : 0)
   }, [searchParams])
 
   const onTabChange = (index: number) => {
     setActiveIndex(index)
-    const tabName = index === 1 ? 'queue' : index === 2 ? 'recent' : 'draft'
-    router.push(`/draft?tab=${tabName}`)
+    router.push(`/draft?tab=${tabs[index].param}`)
   }
 
+  return { activeIndex, onTabChange }
+}
+
+const CreateOwnMarketPage = () => {
   const tabs = [
     {
       title: 'Draft',
       icon: <PlusIcon width={'16px'} height={'16px'} />,
+      component: CreateMarket,
+      param: 'draft',
     },
     {
       title: 'Queue',
       icon: <LoadingIcon width={16} height={16} />,
+      component: DraftMarketsQueue,
+      param: 'queue',
     },
     {
       title: 'Recent',
       icon: <CopyIcon width={16} height={16} />,
+      component: RecentMarkets,
+      param: 'recent',
     },
   ]
-  const tabPanels = [
-    <CreateMarket key={uuidv4()} />,
-    <DraftMarketsQueue key={uuidv4()} />,
-    <RecentMarkets key={uuidv4()} />,
-  ]
+
+  const { activeIndex, onTabChange } = useTabLogic(tabs)
 
   return (
     <MainLayout>
@@ -74,8 +79,10 @@ const CreateOwnMarketPage = () => {
         </TabList>
         <TabIndicator mt='-2px' height='2px' bg='grey.800' transitionDuration='200ms !important' />
         <TabPanels mt='30px'>
-          {tabPanels.map((panel, index) => (
-            <TabPanel key={index}>{panel}</TabPanel>
+          {tabs.map(({ component: Component }, index) => (
+            <TabPanel key={index}>
+              <Component key={uuidv4()} />
+            </TabPanel>
           ))}
         </TabPanels>
       </Tabs>
