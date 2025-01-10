522f0870ef84b2e1d1c2c7ecd55e32d98bb3a84a
ynadtochii <nadtochii.e@gmail.com>
2025-01-10 16:24:33 +0800

    test

--- src/app/portfolio/components/PortfolioPositions.tsx
+++ src/app/portfolio/components/PortfolioPositions.tsx
@@ -61,7 +61,6 @@ const PortfolioPositionsContainer = ({ userMenuLoading }: { userMenuLoading: boo
 
   return (
     <>
-      {/*<Filter onChange={handleSelectFilterTokens} />*/}
       {positionsFiltered.length == 0 ? (
         <Flex w={'full'} h={'200px'} justifyContent={'center'} alignItems={'center'}>
           <Text color={'fontLight'}>No open positions</Text>
