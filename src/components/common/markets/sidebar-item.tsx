98ca7012cba54578967a8fa3860dcc4caeae394e
ynadtochii <nadtochii.e@gmail.com>
2025-01-21 22:55:57 +0800

    hide category if there is no markets on it

--- src/components/common/markets/sidebar-item.tsx
+++ src/components/common/markets/sidebar-item.tsx
@@ -1,14 +1,16 @@
 import { Text, HStack } from '@chakra-ui/react'
 import Link from 'next/link'
 import { useSearchParams } from 'next/navigation'
-import React, { ReactNode } from 'react'
+import React, { ReactNode, useMemo } from 'react'
 import { useTokenFilter } from '@/contexts/TokenFilterContext'
 import Crypto from '@/resources/icons/sidebar/crypto.svg'
 import Finance from '@/resources/icons/sidebar/finance.svg'
 import Others from '@/resources/icons/sidebar/others.svg'
 import Sport from '@/resources/icons/sidebar/sport.svg'
 import Weather from '@/resources/icons/sidebar/weather.svg'
+import { useMarkets } from '@/services/MarketsService'
 import { paragraphMedium } from '@/styles/fonts/fonts.styles'
+import { Market, MarketGroup } from '@/types'
 
 export interface SideItemProps {
   isActive?: boolean
@@ -62,12 +64,37 @@ export const CategoryItems = () => {
   const { selectedCategory, handleCategory } = useTokenFilter()
   const searchParams = useSearchParams()
 
+  const { data } = useMarkets(null)
+
+  const markets: (Market | MarketGroup)[] = useMemo(() => {
+    return data?.pages.flatMap((page) => page.data.markets) || []
+  }, [data?.pages])
+
+  const marketsByCategory = useMemo(() => {
+    if (!markets.length) return {}
+
+    const counts = markets.reduce((acc, market) => {
+      const categoryName =
+        typeof market.category === 'string' ? market.category : market.category.name
+      acc[categoryName] = (acc[categoryName] || 0) + 1
+      return acc
+    }, {} as Record<string, number>)
+
+    return counts
+  }, [markets])
+
   const createQueryString = () => {
     const params = new URLSearchParams(searchParams.toString())
     return params.toString()
   }
 
-  return categories.map((c) => {
+  const categoriesWithMarkets = useMemo(() => {
+    return categories.filter(
+      (category) => marketsByCategory[category.name] && marketsByCategory[category.name] > 0
+    )
+  }, [marketsByCategory])
+
+  return categoriesWithMarkets.map((c) => {
     return (
       <Link key={c.name} href={`/?${createQueryString()}`} style={{ width: '100%' }}>
         <SideItem
@@ -80,7 +107,7 @@ export const CategoryItems = () => {
             })
           }}
         >
-          {c.name}
+          {c.name} ({marketsByCategory[c.name]})
         </SideItem>
       </Link>
     )
