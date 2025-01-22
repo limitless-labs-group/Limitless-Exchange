57ab7c26f84041a4ea30234f7d18be12851d061d
ynadtochii <nadtochii.e@gmail.com>
2025-01-22 23:09:56 +0800

    fixed mobile card

--- src/components/common/markets/market-cards/market-card-trigger.tsx
+++ src/components/common/markets/market-cards/market-card-trigger.tsx
@@ -1,13 +1,10 @@
 import { Box, HStack, Text, VStack } from '@chakra-ui/react'
 import { useRouter } from 'next/navigation'
 import React from 'react'
-import Avatar from '@/components/common/avatar'
 import DailyMarketTimer from '@/components/common/markets/market-cards/daily-market-timer'
 import Paper from '@/components/common/paper'
 import { MarketCardProps } from './market-card-mobile'
 import { MarketProgressBar } from './market-progress-bar'
-import { useMarketFeed } from '@/hooks/use-market-feed'
-import { useUniqueUsersTrades } from '@/hooks/use-unique-users-trades'
 import { ClickEvent, useAmplitude, useTradingService } from '@/services'
 import { headline, paragraphRegular } from '@/styles/fonts/fonts.styles'
 import { NumberUtil } from '@/utils'
@@ -17,9 +14,6 @@ export const MarketCardTrigger = React.memo(
   ({ market, markets, analyticParams }: MarketCardProps) => {
     const { onOpenMarketPage, setMarkets } = useTradingService()
     const router = useRouter()
-    const { data: marketFeedData } = useMarketFeed(market.address)
-
-    const uniqueUsersTrades = useUniqueUsersTrades(marketFeedData)
 
     const { trackClicked } = useAmplitude()
 
@@ -46,12 +40,13 @@ export const MarketCardTrigger = React.memo(
               <Text {...headline} textAlign='start' mt='12px' mb='32px'>
                 {market.title}
               </Text>
-              <MarketProgressBar value={market.prices[0]} />
+              <MarketProgressBar isClosed={market.expired} value={market.prices[0]} />
             </Box>
             <Box w='full'>
               <HStack w='full' justifyContent='space-between'>
                 <Box w='full'>
                   <DailyMarketTimer
+                    hideText
                     deadline={market.expirationTimestamp}
                     deadlineText={market.expirationDate}
                     {...paragraphRegular}
@@ -61,26 +56,7 @@ export const MarketCardTrigger = React.memo(
                 <HStack gap='4px'>
                   <HStack gap='4px'>
                     <>
-                      <HStack gap={0}>
-                        {uniqueUsersTrades?.map(({ user }, index) => (
-                          <Avatar
-                            account={user.account || ''}
-                            avatarUrl={user.imageURI}
-                            key={user.account}
-                            borderColor='grey.100'
-                            zIndex={100 + index}
-                            border='2px solid'
-                            color='grey.100 !important'
-                            showBorder
-                            bg='grey.100'
-                            size='20px'
-                            style={{
-                              border: '2px solid',
-                              marginLeft: index > 0 ? '-6px' : 0,
-                            }}
-                          />
-                        ))}
-                      </HStack>
+                      <HStack gap={0}></HStack>
                       <Text {...paragraphRegular} color='grey.500'>
                         Value
                       </Text>
--- src/components/common/markets/market-cards/market-progress-bar.tsx
+++ src/components/common/markets/market-cards/market-progress-bar.tsx
@@ -11,9 +11,10 @@ const PROGRESS_THRESHOLDS = [
 
 interface MarketProgressBarProps {
   value: number
+  isClosed?: boolean
 }
 
-export const MarketProgressBar = ({ value }: MarketProgressBarProps) => {
+export const MarketProgressBar = ({ isClosed = false, value }: MarketProgressBarProps) => {
   const progressData = useMemo(() => {
     const threshold =
       PROGRESS_THRESHOLDS.find((t) => value <= t.max) ??
@@ -30,20 +31,20 @@ export const MarketProgressBar = ({ value }: MarketProgressBarProps) => {
   return (
     <Box>
       <HStack w='full' mb='4px' justifyContent='space-between'>
-        {yes > 0 && (
+        {yes > 0 && isClosed ? (
           <Flex w='full' justifyContent='start'>
             <Text {...paragraphMedium} color={progressData.color}>
               Yes {yes}%
             </Text>
           </Flex>
-        )}
-        {no > 0 && (
+        ) : null}
+        {no > 0 && isClosed ? (
           <Flex textAlign='left' w='full' justifyContent='end'>
             <Text {...paragraphMedium} color='grey.500'>
               No {no}%
             </Text>
           </Flex>
-        )}
+        ) : null}
       </HStack>
 
       <ProgressBar variant={progressData.variant} size='xs' value={value} />
