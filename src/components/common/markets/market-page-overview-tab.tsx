58ceed3c52fb57462798ec4548d1c74bad446251
ynadtochii <nadtochii.e@gmail.com>
2025-01-23 23:07:00 +0800

    change lumy identification

--- src/components/common/markets/market-page-overview-tab.tsx
+++ src/components/common/markets/market-page-overview-tab.tsx
@@ -24,7 +24,11 @@ export default function MarketPageOverviewTab() {
       )}
       <MarketGroupPredictions />
       <Box w={isMobile ? 'full' : 'fit-content'} mt='16px' pb={isMobile ? '64px' : 0}>
-        {market?.category !== 'Lumy' ? (
+        {market?.tags.includes('Lumy') ? (
+          <Link variant='textLinkSecondary' {...paragraphRegular} isExternal color='grey.500'>
+            Resolution is decentralised
+          </Link>
+        ) : (
           <>
             <NextLink
               href='https://www.notion.so/limitlesslabs/Limitless-Docs-0e59399dd44b492f8d494050969a1567?pvs=4#5dd6f962c66044eaa00e28d2c61b92bb'
@@ -41,10 +45,6 @@ export default function MarketPageOverviewTab() {
               and made by the Limitless team
             </Text>
           </>
-        ) : (
-          <Link variant='textLinkSecondary' {...paragraphRegular} isExternal color='grey.500'>
-            Resolution is decentralised
-          </Link>
         )}
 
         <Text mt='16px' {...paragraphRegular}>
