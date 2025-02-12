10a4c6a945e0c98306ea786e96ad5f010bd1deac
oleksandrtarapata <oleksandr@limitless.network>
2025-02-12 16:42:50 +0000

    temp commit

--- src/components/common/markets/activity/activity-clob-item.tsx
+++ src/components/common/markets/activity/activity-clob-item.tsx
@@ -16,12 +16,12 @@ export default function ActivityClobItem({ data }: ActivityClobItemProps) {
   const title = useMemo(() => {
     const title = data.side === 0 ? 'Bought' : 'Sold'
     const outcome = market?.tokens.yes === data.tokenId ? 'Yes' : 'No'
-    const contracts = formatUnits(BigInt(data.matchedSize), market?.collateralToken.decimals || 6)
+    const totalAmount = formatUnits(BigInt(data.takerAmount), market?.collateralToken.decimals || 6)
     return `${title} ${NumberUtil.toFixed(
       formatUnits(BigInt(data.matchedSize), market?.collateralToken.decimals || 6),
       6
     )} contracts ${outcome} for ${NumberUtil.toFixed(
-      Math.abs(new BigNumber(contracts).multipliedBy(data.price).toNumber()),
+      Math.abs(new BigNumber(totalAmount).toNumber()),
       market?.collateralToken.symbol === 'USDC' ? 2 : 6
     )} ${market?.collateralToken.symbol} in total.`
   }, [market, data])
