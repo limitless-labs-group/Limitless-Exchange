06447fa66cc9d878dbce9420b756b8b7eff3be45
ynadtochii <nadtochii.e@gmail.com>
2025-02-17 12:03:08 +0000

    make reward icon visible by default

--- src/app/(markets)/markets/[address]/components/clob/orderbook-table-large.tsx
+++ src/app/(markets)/markets/[address]/components/clob/orderbook-table-large.tsx
@@ -181,53 +181,55 @@ export default function OrderbookTableLarge({
       <HStack w='full' justifyContent='space-between' mb='14px'>
         <Text {...h3Regular}>Order book</Text>
         <HStack gap='16px'>
-          {market?.isRewardable && (
-            <Box position='relative'>
-              <HStack
-                gap='4px'
-                borderRadius='8px'
-                py='4px'
-                px='8px'
-                bg={rewardsButtonClicked ? 'blue.500' : 'blueTransparent.100'}
-                cursor='pointer'
-                onClick={handleRewardsClicked}
-                onMouseEnter={() => {
-                  const timer = setTimeout(() => {
-                    setRewardButtonHovered(true)
-                  }, 300)
-                  return () => clearTimeout(timer)
-                }}
-                onMouseLeave={() => setRewardButtonHovered(false)}
-                ref={ref as LegacyRef<HTMLDivElement>}
+          <Box
+            position='relative'
+            opacity={market?.isRewardable ? 1 : 0}
+            pointerEvents={market?.isRewardable ? 'auto' : 'none'}
+          >
+            <HStack
+              gap='4px'
+              borderRadius='8px'
+              py='4px'
+              px='8px'
+              bg={rewardsButtonClicked ? 'blue.500' : 'blueTransparent.100'}
+              cursor='pointer'
+              onClick={handleRewardsClicked}
+              onMouseEnter={() => {
+                const timer = setTimeout(() => {
+                  setRewardButtonHovered(true)
+                }, 300)
+                return () => clearTimeout(timer)
+              }}
+              onMouseLeave={() => setRewardButtonHovered(false)}
+              ref={ref as LegacyRef<HTMLDivElement>}
+            >
+              <GemIcon />
+              <Text {...paragraphMedium} color={rewardsButtonClicked ? 'white' : 'blue.500'}>
+                {marketRewards && Boolean(marketRewards?.length)
+                  ? `Earnings ${NumberUtil.toFixed(marketRewards[0].totalUnpaidReward, 6)} ${
+                      market?.collateralToken.symbol
+                    }`
+                  : 'Earn Rewards'}
+              </Text>
+            </HStack>
+            {(rewardsButtonClicked || rewardButtonHovered) && (
+              <Box
+                position='absolute'
+                bg='grey.50'
+                border='1px solid'
+                borderColor='grey.200'
+                boxShadow='0px 1px 4px 0px rgba(2, 6, 23, 0.05)'
+                w='260px'
+                p='8px'
+                rounded='8px'
+                right={0}
+                minH='128px'
+                zIndex={150}
               >
-                <GemIcon />
-                <Text {...paragraphMedium} color={rewardsButtonClicked ? 'white' : 'blue.500'}>
-                  {marketRewards && Boolean(marketRewards?.length)
-                    ? `Earnings ${NumberUtil.toFixed(marketRewards[0].totalUnpaidReward, 6)} ${
-                        market?.collateralToken.symbol
-                      }`
-                    : 'Earn Rewards'}
-                </Text>
-              </HStack>
-              {(rewardsButtonClicked || rewardButtonHovered) && (
-                <Box
-                  position='absolute'
-                  bg='grey.50'
-                  border='1px solid'
-                  borderColor='grey.200'
-                  boxShadow='0px 1px 4px 0px rgba(2, 6, 23, 0.05)'
-                  w='260px'
-                  p='8px'
-                  rounded='8px'
-                  right={0}
-                  minH='128px'
-                  zIndex={150}
-                >
-                  {tooltipContent}
-                </Box>
-              )}
-            </Box>
-          )}
+                {tooltipContent}
+              </Box>
+            )}
+          </Box>
           <HStack w={'152px'} bg='grey.200' borderRadius='8px' py='2px' px={'2px'}>
             <Button
               h={isMobile ? '28px' : '20px'}
