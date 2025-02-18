01022cff558647273d665bc63d878d21e023a229
Anton Sakhniuk <anton.sakhniuk@gmail.com>
2025-02-18 17:36:04 +0100

    fix: feed request missing auth token

--- src/hooks/use-client.ts
+++ src/hooks/use-client.ts
@@ -5,10 +5,9 @@ import { LOGGED_IN_TO_LIMITLESS } from '@/utils/consts'
 export default function useClient() {
   const privateClient = useAxiosPrivateClient()
   const isLogged = localStorage.getItem(LOGGED_IN_TO_LIMITLESS)
-  const { web3Wallet } = useAccount()
 
   return {
     isLogged,
-    client: web3Wallet ? privateClient : limitlessApi,
+    client: isLogged ? privateClient : limitlessApi,
   }
 }
