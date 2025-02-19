16863116b14756435d76ff1143fa31f6f318c6c0
valentynkvasov <valentynkvasov@gmail.com>
2025-02-19 16:49:19 +0000

    hotfix: revert block

--- src/hooks/use-client.ts
+++ src/hooks/use-client.ts
@@ -1,13 +1,14 @@
-import { limitlessApi } from '@/services'
+import { limitlessApi, useAccount } from '@/services'
 import { useAxiosPrivateClient } from '@/services/AxiosPrivateClient'
 import { LOGGED_IN_TO_LIMITLESS } from '@/utils/consts'
 
 export default function useClient() {
   const privateClient = useAxiosPrivateClient()
   const isLogged = localStorage.getItem(LOGGED_IN_TO_LIMITLESS)
+  const { web3Wallet } = useAccount()
 
   return {
     isLogged,
-    client: isLogged ? privateClient : limitlessApi,
+    client: web3Wallet ? privateClient : limitlessApi,
   }
 }
