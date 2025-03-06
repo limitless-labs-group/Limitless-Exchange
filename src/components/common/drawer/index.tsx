bbdd8b86898aa1f02700ff3e5a575ca09203c432
oleksandrtarapata <oleksandr@limitless.network>
2025-03-06 17:13:39 +0000

    minor fix

--- src/components/common/drawer/index.tsx
+++ src/components/common/drawer/index.tsx
@@ -159,113 +159,77 @@ export default function MobileDrawer({
   )
 
   return (
-    <Drawer.Root>
-      <Drawer.Trigger>
+    <Drawer.Root shouldScaleBackground onClose={close}>
+      <Drawer.Trigger asChild>
         <button style={{ width: '100%', ...triggerStyle }} ref={drawerRef}>
           {trigger}
         </button>
       </Drawer.Trigger>
       <Drawer.Portal>
-        <Drawer.Overlay className='fixed inset-0 bg-black/40' />
-        <Drawer.Content className='bg-white flex flex-col fixed bottom-0 left-0 right-0 max-h-[82vh] rounded-t-[10px]'>
-          <div className='max-w-md w-full mx-auto overflow-auto p-4 rounded-t-[10px]'>
-            <Drawer.Handle />
-            <Drawer.Title className='font-medium text-gray-900 mt-8'>New Project</Drawer.Title>
-            <Drawer.Description className='leading-6 mt-2 text-gray-600'>
-              Get started by filling in the information below to create your new project.
-            </Drawer.Description>
-            <label htmlFor='name' className='font-medium text-gray-900 text-sm mt-8 mb-2 block'>
-              Project name
-            </label>
-            <input
-              id='name'
-              className='border border-gray-200 bg-white w-full px-3 h-9 rounded-lg outline-none focus:ring-2 focus:ring-black/5 text-gray-900'
+        <Drawer.Overlay
+          style={{
+            position: 'fixed',
+            inset: 0,
+            background: 'rgba(0, 0, 0, 0.3)',
+            zIndex: 99999,
+          }}
+        />
+        <Drawer.Content style={drawerStyle}>
+          <div
+            style={{
+              flex: 1,
+            }}
+          >
+            <div
+              style={{
+                margin: '8px auto',
+                width: '36px',
+                height: '4px',
+                borderRadius: '8px',
+                background: grabberBgColor,
+              }}
             />
-            <label htmlFor='name' className='font-medium text-gray-900 text-sm mt-8 mb-2 block'>
-              Description
-            </label>
-            <textarea
-              rows={6}
-              className='border border-gray-200 bg-white w-full resize-none rounded-lg p-3 pt-2.5 text-gray-900 outline-none focus:ring-2 focus:ring-black/5 focus:ring-offset-0'
-            />
-            <button className='h-[44px] bg-black text-gray-50 rounded-lg mt-4 w-full font-medium'>
-              Submit
-            </button>
+            {!!onClickPrevious || !!onClickNext ? (
+              <HStack w='full' justifyContent='space-between'>
+                {onClickPrevious ? (
+                  <Button variant='transparentGreyText' onClick={onClickPrevious}>
+                    <ArrowLeftIcon width={24} height={24} />
+                    Previous
+                  </Button>
+                ) : (
+                  <div />
+                )}
+                {onClickNext ? (
+                  <Button variant='transparentGreyText' onClick={onClickNext}>
+                    Next
+                    <ArrowRightIcon width={24} height={24} />
+                  </Button>
+                ) : (
+                  <div />
+                )}
+              </HStack>
+            ) : null}
+            <div style={contentStyle}>
+              <>
+                {title && (
+                  <Drawer.Title
+                    style={{
+                      marginBottom: '32px',
+                      marginTop: '28px',
+                      padding: '0 16px',
+                      ...(variant === 'blue' ? { ...headline } : { ...h1Regular }),
+                      color: titleColor,
+                    }}
+                  >
+                    <>{title}</>
+                  </Drawer.Title>
+                )}
+              </>
+              <>{children}</>
+            </div>
           </div>
         </Drawer.Content>
       </Drawer.Portal>
     </Drawer.Root>
-    // <Drawer.Root shouldScaleBackground onClose={close}>
-    //   <Drawer.Trigger asChild>
-    //     <button style={{ width: '100%', ...triggerStyle }} ref={drawerRef}>
-    //       {trigger}
-    //     </button>
-    //   </Drawer.Trigger>
-    //   <Drawer.Portal>
-    //     <Drawer.Overlay
-    //       style={{
-    //         position: 'fixed',
-    //         inset: 0,
-    //         background: 'rgba(0, 0, 0, 0.3)',
-    //         zIndex: 99999,
-    //       }}
-    //     />
-    //     <Drawer.Content style={drawerStyle}>
-    //       <div
-    //         style={{
-    //           flex: 1,
-    //         }}
-    //       >
-    //         <div
-    //           style={{
-    //             margin: '8px auto',
-    //             width: '36px',
-    //             height: '4px',
-    //             borderRadius: '8px',
-    //             background: grabberBgColor,
-    //           }}
-    //         />
-    //         {!!onClickPrevious || !!onClickNext ? (
-    //           <HStack w='full' justifyContent='space-between'>
-    //             {onClickPrevious ? (
-    //               <Button variant='transparentGreyText' onClick={onClickPrevious}>
-    //                 <ArrowLeftIcon width={24} height={24} />
-    //                 Previous
-    //               </Button>
-    //             ) : (
-    //               <div />
-    //             )}
-    //             {onClickNext ? (
-    //               <Button variant='transparentGreyText' onClick={onClickNext}>
-    //                 Next
-    //                 <ArrowRightIcon width={24} height={24} />
-    //               </Button>
-    //             ) : (
-    //               <div />
-    //             )}
-    //           </HStack>
-    //         ) : null}
-    //         <div style={contentStyle}>
-    //           <>
-    //             {title && (
-    //               <Drawer.Title
-    //                 style={{
-    //                   marginBottom: '32px',
-    //                   marginTop: '28px',
-    //                   padding: '0 16px',
-    //                   ...(variant === 'blue' ? { ...headline } : { ...h1Regular }),
-    //                   color: titleColor,
-    //                 }}
-    //               >
-    //                 <>{title}</>
-    //               </Drawer.Title>
-    //             )}
-    //           </>
-    //           <>{children}</>
-    //         </div>
-    //       </div>
-    //     </Drawer.Content>
-    //   </Drawer.Portal>
-    // </Drawer.Root>
   )
 }
