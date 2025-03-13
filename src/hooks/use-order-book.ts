3f7c451de9209c58b0f13fc343311f1ad39e40e7
oleksandrtarapata <oleksandr@limitless.network>
2025-03-13 11:17:48 +0100

    temp commit

--- src/hooks/use-order-book.ts
+++ src/hooks/use-order-book.ts
@@ -22,59 +22,13 @@ export function useOrderBook(slug?: string) {
   return useQuery({
     queryKey: ['order-book', slug],
     queryFn: async () => {
+      const response: AxiosResponse<OrderBook> = await limitlessApi.get(
+        `/markets/${slug}/orderbook`
+      )
       return {
-        bids: [
-          {
-            price: 0.12,
-            size: 1127723183,
-            side: 'BUY',
-          },
-          {
-            price: 0.105,
-            size: 2500000000,
-            side: 'BUY',
-          },
-          {
-            price: 0.08,
-            size: 202000000,
-            side: 'BUY',
-          },
-          {
-            price: 0.01,
-            size: 2000000000,
-            side: 'BUY',
-          },
-        ],
-        asks: [
-          {
-            price: 0.15,
-            size: 100000000,
-            side: 'SELL',
-          },
-          {
-            price: 0.38,
-            size: 100000000,
-            side: 'SELL',
-          },
-          {
-            price: 0.99,
-            size: 1000000000,
-            side: 'SELL',
-          },
-        ],
-        tokenId: '67354934772933660976333072373492305909519455738242108659979302147764959766222',
-        lastTradePrice: 0.11,
-        adjustedMidpoint: 0.135,
-        maxSpread: '0.03',
-        minSize: '100000000',
+        ...response.data,
+        maxSpread: new BigNumber(response.data.maxSpread).minus('0.005').toString(),
       }
-      // const response: AxiosResponse<OrderBook> = await limitlessApi.get(
-      //   `/markets/${slug}/orderbook`
-      // )
-      // return {
-      //   ...response.data,
-      //   maxSpread: new BigNumber(response.data.maxSpread).minus('0.005').toString(),
-      // }
       // return {
       //   ...response.data,
       //   asks: response.data.asks.filter((ask) => {
