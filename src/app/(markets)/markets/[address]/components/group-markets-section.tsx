802f968347f15d6574bfc00008ea15bbc0a62ee6
oleksandrtarapata <oleksandr@limitless.network>
2025-03-25 11:18:10 +0000

    added scroll to orders table

--- src/app/(markets)/markets/[address]/components/group-markets-section.tsx
+++ src/app/(markets)/markets/[address]/components/group-markets-section.tsx
@@ -1,5 +1,7 @@
 import { Accordion } from '@chakra-ui/react'
+import { isMobile } from 'react-device-detect'
 import GroupMarketSectionDesktop from '@/app/(markets)/markets/[address]/components/group-market-section-desktop'
+import GroupMarketSectionSmall from '@/app/(markets)/markets/[address]/components/group-market-section-small'
 import GroupMarketsSectionMobile from '@/app/(markets)/markets/[address]/components/group-markets-section-mobile'
 import { useTradingService } from '@/services'
 
@@ -10,15 +12,18 @@ interface GroupMarketsSectionProps {
 export default function GroupMarketsSection({ mobileView }: GroupMarketsSectionProps) {
   const { groupMarket } = useTradingService()
 
-  const cards = groupMarket?.markets?.map((market) =>
-    mobileView ? (
-      <GroupMarketsSectionMobile key={market.slug} market={market} />
+  const cards = groupMarket?.markets?.map((market) => {
+    if (isMobile) {
+      return <GroupMarketsSectionMobile key={market.slug} market={market} />
+    }
+    return mobileView ? (
+      <GroupMarketSectionSmall key={market.slug} market={market} />
     ) : (
       <GroupMarketSectionDesktop key={market.slug} market={market} />
     )
-  )
+  })
 
-  return mobileView ? (
+  return isMobile ? (
     <>{cards}</>
   ) : (
     <Accordion variant='paper' gap='8px' display='flex' flexDirection='column' allowToggle>
