adbc63d32405f3c20bff7e5f27783e5ef7eb2cf9
ynadtochii <nadtochii.e@gmail.com>
2025-04-08 18:35:36 +0100

    block

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -2,3 +2,7 @@ export const DISCORD_LINK = 'https://discord.gg/aGSS8zswwB'
 
 export const LOGGED_IN_TO_LIMITLESS = 'logged-in-to-limitless'
 export const USER_ID = 'id'
+
+export const INFO_MSG = 'Market is locked. Trading stopped. Please await for final resolution.'
+export const TRADING_BLOCKED_MSG =
+  'Trading is unavailable to individuals or companies based in the U.S. or restricted territories.'
