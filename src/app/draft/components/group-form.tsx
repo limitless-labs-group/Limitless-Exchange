b4c9fe1317fe00a34182aeb7afa5efd84428dd1c
oleksandrtarapata <oleksandr@limitless.network>
2025-04-17 13:33:26 +0100

    fixed text area shrink while input long description to negrisk

--- src/app/draft/components/group-form.tsx
+++ src/app/draft/components/group-form.tsx
@@ -138,6 +138,9 @@ export const GroupForm = () => {
                   handleInputChange(index, 'description', value)
                 }
               }}
+              style={{
+                wordBreak: 'break-all',
+              }}
             />
           </FormControl>
           <Flex w='full' direction='row' gap={2} flexWrap='wrap' alignItems='start'>
