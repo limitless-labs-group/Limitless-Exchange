3497202a75dfb914203d166fc28cd68cc7348839
ynadtochii <nadtochii.e@gmail.com>
2025-04-23 16:02:49 +0100

    fixed prio index

--- src/app/draft/components/use-create-market.tsx
+++ src/app/draft/components/use-create-market.tsx
@@ -285,13 +285,13 @@ export const useCreateMarket = () => {
       categoryIds: matchedCategoryIds,
       ogFile: ogLogo as File | null,
       tagIds: matchedTagIds,
+      priorityIndex: priorityIndex ?? undefined,
     }
 
     if (isClob) {
       return {
         ...baseData,
         description: description ?? '',
-        priorityIndex: priorityIndex ?? undefined,
         minSize: formData.minSize !== undefined ? calculateMinSize(formData.minSize) : undefined,
         maxSpread: calculateMaxSpread(formData.maxSpread),
         c: formData.c,
@@ -314,7 +314,6 @@ export const useCreateMarket = () => {
     } else {
       return {
         ...baseData,
-        priorityIndex: priorityIndex ?? undefined,
         description: description ?? '',
         liquidity: formData.liquidity ?? 0,
         initialYesProbability: formData.probability ? formData.probability / 100 : 0,
@@ -345,13 +344,13 @@ export const useCreateMarket = () => {
       categoryIds: matchedCategoryIds,
       tagIds: matchedTagIds,
       deadline: String(calculateZonedTime(deadline, timezone)),
+      priorityIndex: formData.priorityIndex,
     }
 
     if (isClob) {
       return {
         ...baseData,
         description: formData.description ?? '',
-        priorityIndex: formData.priorityIndex,
         minSize: calculateMinSize(Number(formData.minSize)),
         maxSpread: calculateMaxSpread(Number(formData.maxSpread)),
         c: Number(formData.c),
@@ -374,7 +373,6 @@ export const useCreateMarket = () => {
     } else {
       return {
         ...baseData,
-        priorityIndex: formData.priorityIndex,
         description: formData.description ?? '',
       }
     }
