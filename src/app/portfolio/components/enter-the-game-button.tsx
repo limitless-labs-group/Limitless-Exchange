e8ec3e4b4e80a4bab664b0cf85cc665daeba816c
oleksandrtarapata <oleksandr@limitless.network>
2025-04-29 17:52:26 +0100

    temp commit

--- src/app/portfolio/components/enter-the-game-button.tsx
+++ src/app/portfolio/components/enter-the-game-button.tsx
@@ -1,12 +1,17 @@
 import { Button } from '@chakra-ui/react'
 import { sleep } from '@etherspot/prime-sdk/dist/sdk/common'
+import { useQueryClient } from '@tanstack/react-query'
 import Cookies from 'js-cookie'
 import { useState } from 'react'
 import { ClickEvent, useAmplitude } from '@/services'
+import { useAxiosPrivateClient } from '@/services/AxiosPrivateClient'
 
 export default function EnterTheGameButton() {
+  const queryClient = useQueryClient()
   const [showNewMessage, setShowNewMessage] = useState<boolean>(false)
   const [hideButton, setHideButton] = useState<boolean>(!!Cookies.get('points-button-clicked'))
+
+  const privateClient = useAxiosPrivateClient()
 
   const { trackClicked } = useAmplitude()
 
@@ -15,9 +20,12 @@ export default function EnterTheGameButton() {
     trackClicked(ClickEvent.PointsButtonClicked, {
       page: 'Portfolio Page',
     })
+    await privateClient.post('points/enroll')
     await sleep(5)
-    Cookies.set('points-button-clicked', 'true')
     setHideButton(true)
+    await queryClient.refetchQueries({
+      queryKey: ['portfolio'],
+    })
   }
 
   return hideButton ? null : (
