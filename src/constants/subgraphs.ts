a2b5c8f2a50fe30a54721013e9ad2f0bd6355d51
oleksandrtarapata <oleksandr@limitless.network>
2025-05-03 11:57:43 +0100

    temp commit

--- src/constants/subgraphs.ts
+++ src/constants/subgraphs.ts
@@ -1,6 +1,6 @@
 import { base, baseSepolia } from 'viem/chains'
 
 export const newSubgraphURI = {
-  [base.id]: 'https://indexer.bigdevenergy.link/da7c4d3/v1/graphql',
+  [base.id]: 'https://indexer.hyperindex.xyz/da7c4d3/v1/graphql',
   [baseSepolia.id]: 'https://indexer.bigdevenergy.link/e46ec4a/v1/graphql',
 }
