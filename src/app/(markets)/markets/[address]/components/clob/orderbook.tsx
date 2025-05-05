471f30f56761cdb444deb9e1981ed6d118022b55
oleksandrtarapata <oleksandr@limitless.network>
2025-05-05 09:53:57 +0100

    temp commit

--- src/app/(markets)/markets/[address]/components/clob/orderbook.tsx
+++ src/app/(markets)/markets/[address]/components/clob/orderbook.tsx
@@ -1,6 +1,6 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import BigNumber from 'bignumber.js'
-import React, { useCallback, useMemo } from 'react'
+import React, { useMemo } from 'react'
 import { isMobile } from 'react-device-detect'
 import OrderbookTableLarge from '@/app/(markets)/markets/[address]/components/clob/orderbook-table-large'
 import OrderBookTableSmall from '@/app/(markets)/markets/[address]/components/clob/orderbook-table-small'
@@ -42,10 +42,10 @@ export default function Orderbook({ variant }: OrderBookProps) {
     },
   })
 
-  const orderbookData = useCallback(
+  const orderbookData = useMemo(
     () => getOrderBookData(outcome, orderbook, market?.collateralToken.decimals),
-    [outcome]
-  )()
+    [outcome, orderbook]
+  )
 
   const spread = useMemo(() => {
     if (!orderbookData) {
