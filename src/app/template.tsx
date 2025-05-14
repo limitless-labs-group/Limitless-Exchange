9ef0a94d7a1196fa4546019887ae870ad069c0ab
ynadtochii <nadtochii.e@gmail.com>
2025-05-14 14:08:11 +0100

    fixed chat on mobile

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -2,10 +2,23 @@
 
 import Intercom from '@intercom/messenger-js-sdk'
 import { PropsWithChildren, useEffect } from 'react'
+import { isMobile } from 'react-device-detect'
 import { LOCAL_STORAGE_VERSION_NAME } from '@/constants/application'
 import { useTotalTradingVolume } from '@/hooks/use-total-trading-volume'
 import { useAccount } from '@/services'
 import packageInfo from '../../package.json'
+
+export const openIntercom = () => {
+  if (typeof window !== 'undefined' && window.Intercom) {
+    window.Intercom('show')
+  }
+}
+
+export const closeIntercom = () => {
+  if (typeof window !== 'undefined' && window.Intercom) {
+    window.Intercom('hide')
+  }
+}
 
 export default function Template({ children }: PropsWithChildren) {
   const { disconnectFromPlatform } = useAccount()
@@ -13,10 +26,21 @@ export default function Template({ children }: PropsWithChildren) {
 
   const currentAppVersion = packageInfo.version
 
-  Intercom({
-    app_id: process.env.NEXT_PUBLIC_INTERCOM_APP_ID as string,
-    action_color: '#0079FF',
-  })
+  useEffect(() => {
+    if (!isMobile) {
+      Intercom({
+        app_id: process.env.NEXT_PUBLIC_INTERCOM_APP_ID as string,
+        action_color: '#0079FF',
+        hide_default_launcher: false,
+      })
+    } else {
+      Intercom({
+        app_id: process.env.NEXT_PUBLIC_INTERCOM_APP_ID as string,
+        action_color: '#0079FF',
+        hide_default_launcher: true, // Hide the default launcher on mobile
+      })
+    }
+  }, [])
 
   // useEffect(() => {
   //   const currentUsersVersion = localStorage.getItem(LOCAL_STORAGE_VERSION_NAME)
